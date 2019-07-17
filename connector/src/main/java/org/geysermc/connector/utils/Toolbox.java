c85e5b019fb657b3a55ee82fd863f55b03747eac
RednedEpic <redned@retronixgames.x10host.com>
2019-07-17 13:00:11 -0500

    Code cleanups

--- connector/src/main/java/org/geysermc/connector/utils/Toolbox.java
+++ connector/src/main/java/org/geysermc/connector/utils/Toolbox.java
@@ -1,8 +1,31 @@
+/*
+ * Copyright (c) 2019 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/Geyser
+ */
+
 package org.geysermc.connector.utils;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
 import com.nukkitx.network.VarInts;
 import com.nukkitx.protocol.bedrock.v361.BedrockUtils;
 import io.netty.buffer.ByteBuf;
@@ -10,18 +33,16 @@ import io.netty.buffer.Unpooled;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Toolbox {
+
     static {
-
         InputStream stream = Toolbox.class.getClassLoader().getResourceAsStream("cached_pallete.json");
-
         ObjectMapper mapper = new ObjectMapper();
-
-        ArrayList<LinkedHashMap<String, Object>> entries = new ArrayList<>();
+        List<LinkedHashMap<String, Object>> entries = new ArrayList<>();
 
         try {
             entries = mapper.readValue(stream, ArrayList.class);
@@ -30,16 +51,13 @@ public class Toolbox {
         }
 
         ByteBuf b = Unpooled.buffer();
-
         VarInts.writeInt(b, entries.size());
-
         for (Map<String, Object> e : entries) {
             BedrockUtils.writeString(b, (String) e.get("name"));
             b.writeShortLE((Integer) e.get("data"));
         }
 
         CACHED_PALLETE = b;
-
     }
 
     public static final ByteBuf CACHED_PALLETE;
