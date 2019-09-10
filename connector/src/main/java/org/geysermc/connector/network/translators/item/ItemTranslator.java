6cb80dea280ac9ab2391e2820a13ed2353f8ab4d
RednedEpic <redned235@gmail.com>
2019-09-10 18:45:46 -0500

    Minor cleanup upon initial block remap

--- connector/src/main/java/org/geysermc/connector/network/translators/item/ItemTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/item/ItemTranslator.java
@@ -99,7 +99,7 @@ public class ItemTranslator {
     public BedrockItem getBedrockBlock(BlockState state) {
         BedrockItem bedrockItem = Remapper.BLOCK_REMAPPER.convertToBedrock(new ItemStack(state.getId()));
         if (bedrockItem == null) {
-            //GeyserLogger.DEFAULT.debug("Missing mapping for java item " + state.getId());
+            // GeyserLogger.DEFAULT.debug("Missing mapping for java block " + state.getId());
             return BedrockItem.AIR;
         }
 
