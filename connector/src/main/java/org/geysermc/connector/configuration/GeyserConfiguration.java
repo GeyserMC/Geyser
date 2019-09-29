c8404d15c1b5de60d7e3f37ae1be01ae03c5b7a5
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-09-29 20:05:41 +0200

    fix that unknown Configuration crashes geyser

--- connector/src/main/java/org/geysermc/connector/configuration/GeyserConfiguration.java
+++ connector/src/main/java/org/geysermc/connector/configuration/GeyserConfiguration.java
@@ -25,11 +25,13 @@
 
 package org.geysermc.connector.configuration;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class GeyserConfiguration {
 
