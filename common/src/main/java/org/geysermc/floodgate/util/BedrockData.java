9a13b566cff7ab84e4dc37ed446b111794368a4a
Tim203 <mctim203@gmail.com>
2019-11-30 21:30:25 +0100

    Little changes

---
+++ common/src/main/java/org/geysermc/floodgate/util/BedrockData.java
@@ -0,0 +1,35 @@
+package org.geysermc.floodgate.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BedrockData {
+    public static final int EXPECTED_LENGTH = 6;
+    private String version;
+    private String username;
+    private String xuid;
+    private int deviceId;
+    private String languageCode;
+    private int inputMode;
+    private int dataLength;
+
+    public BedrockData(String version, String username, String xuid, int deviceId, String languageCode, int inputMode) {
+        this(version, username, xuid, deviceId, languageCode, inputMode, EXPECTED_LENGTH);
+    }
+
+    public static BedrockData fromString(String data) {
+        String[] split = data.split("\0");
+        if (split.length != EXPECTED_LENGTH) return null;
+        return new BedrockData(split[0], split[1], split[2], Integer.parseInt(split[3]), split[4], Integer.parseInt(split[5]), split.length);
+    }
+
+    public static BedrockData fromRawData(byte[] data) {
+        return fromString(new String(data));
+    }
+
+    public String toString() {
+        return version +'\0'+ username +'\0'+ xuid +'\0'+ deviceId +'\0'+ languageCode +'\0'+ inputMode;
+    }
+}
