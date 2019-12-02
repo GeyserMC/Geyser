4310abd112ab209c5ee3426cf1e25614303f3336
LegacyGamerHD <wesleyvanneck105@outlook.com>
2019-12-02 15:11:45 +0100

    Update GeyserBungeeConfiguration.java

--- bootstrap/bungeecord/src/main/java/org/geysermc/platform/bungeecord/GeyserBungeeConfiguration.java
+++ bootstrap/bungeecord/src/main/java/org/geysermc/platform/bungeecord/GeyserBungeeConfiguration.java
@@ -31,7 +31,9 @@ import org.geysermc.common.IGeyserConfiguration;
 
 import java.util.HashMap;
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+ @JsonIgnoreProperties(ignoreUnknown = true)
 public class GeyserBungeeConfiguration implements IGeyserConfiguration {
 
     private Configuration config;
