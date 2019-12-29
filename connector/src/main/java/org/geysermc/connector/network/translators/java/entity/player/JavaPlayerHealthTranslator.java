377a87f7e31b321f0d75d3b7967e66b57d1e8974
AJ Ferguson <fergusonjva@gmail.com>
2019-12-29 01:45:57 -0900

    Fix health edge case
    If the java server sent a health value between 0 and 1, the player would die in the bedrock client

--- connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerHealthTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerHealthTranslator.java
@@ -40,8 +40,9 @@ public class JavaPlayerHealthTranslator extends PacketTranslator<ServerPlayerHea
         if (entity == null)
             return;
 
+        int health = (int) Math.ceil(packet.getHealth());
         SetHealthPacket setHealthPacket = new SetHealthPacket();
-        setHealthPacket.setHealth((int) Math.ceil(packet.getHealth()));
+        setHealthPacket.setHealth(health);
         session.getUpstream().sendPacket(setHealthPacket);
 
         float maxHealth = entity.getAttributes().containsKey(AttributeType.MAX_HEALTH) ? entity.getAttributes().get(AttributeType.MAX_HEALTH).getValue() : 20f;
@@ -50,7 +51,7 @@ public class JavaPlayerHealthTranslator extends PacketTranslator<ServerPlayerHea
             maxHealth += 1;
         }
 
-        entity.getAttributes().put(AttributeType.HEALTH, AttributeType.HEALTH.getAttribute(packet.getHealth(), maxHealth));
+        entity.getAttributes().put(AttributeType.HEALTH, AttributeType.HEALTH.getAttribute(health, maxHealth));
         entity.getAttributes().put(AttributeType.HUNGER, AttributeType.HUNGER.getAttribute(packet.getFood()));
         entity.getAttributes().put(AttributeType.SATURATION, AttributeType.SATURATION.getAttribute(packet.getSaturation()));
         entity.updateBedrockAttributes(session);
