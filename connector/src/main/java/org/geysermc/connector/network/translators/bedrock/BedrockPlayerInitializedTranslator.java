6702c59a15bb194a48b13a634003ea162ff0fc25
RednedEpic <redned235@gmail.com>
2020-01-18 16:50:17 -0600

    Fix auth type check from merge conflict

--- connector/src/main/java/org/geysermc/connector/network/translators/bedrock/BedrockPlayerInitializedTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/bedrock/BedrockPlayerInitializedTranslator.java
@@ -26,6 +26,8 @@
 package org.geysermc.connector.network.translators.bedrock;
 
 import com.nukkitx.protocol.bedrock.packet.SetLocalPlayerAsInitializedPacket;
+
+import org.geysermc.common.AuthType;
 import org.geysermc.connector.entity.PlayerEntity;
 import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.PacketTranslator;
@@ -38,7 +40,7 @@ public class BedrockPlayerInitializedTranslator extends PacketTranslator<SetLoca
             if (!session.getUpstream().isInitialized()) {
                 session.getUpstream().setInitialized(true);
 
-                if (!(session.getConnector().getConfig().getRemote().getAuthType().hashCode() == "online".hashCode())) {
+                if (!(session.getConnector().getAuthType() == AuthType.OFFLINE)) {
                     session.getConnector().getLogger().info("Attempting to login using offline mode... authentication is disabled.");
                     session.authenticate(session.getAuthData().getName());
                 }
