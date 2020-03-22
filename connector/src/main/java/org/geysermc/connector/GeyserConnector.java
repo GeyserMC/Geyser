1ec9352cdcde76e0caf36d8c942f16923521a5e4
Ender <33337309+e3ndr@users.noreply.github.com>
2020-03-21 21:58:00 -0500

    Forgot to change RELEASE to false.

--- connector/src/main/java/org/geysermc/connector/GeyserConnector.java
+++ connector/src/main/java/org/geysermc/connector/GeyserConnector.java
@@ -60,7 +60,7 @@ public class GeyserConnector {
 
     public static final String NAME = "Geyser";
     public static final String VERSION = "1.0-SNAPSHOT";
-    public static final boolean RELEASE = true;
+    public static final boolean RELEASE = false;
 
     private final Map<Object, GeyserSession> players = new HashMap<>();
 
