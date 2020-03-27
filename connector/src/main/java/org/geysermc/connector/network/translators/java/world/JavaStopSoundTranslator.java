2318e4574f2fef19c8d391f0451bce1409225a25
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2020-03-27 20:22:22 +0100

    It's fxxking crazy
    now player can hear sounds

---
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/world/JavaStopSoundTranslator.java
@@ -0,0 +1,21 @@
+package org.geysermc.connector.network.translators.java.world;
+
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerStopSoundPacket;
+import com.nukkitx.protocol.bedrock.packet.StopSoundPacket;
+import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.network.translators.PacketTranslator;
+import org.geysermc.connector.utils.SoundUtils;
+
+public class JavaStopSoundTranslator extends PacketTranslator<ServerStopSoundPacket> {
+
+    @Override
+    public void translate(ServerStopSoundPacket packet, GeyserSession session) {
+        String identifier = SoundUtils.getIdentifier(packet.getSound());
+        if(identifier != null){
+            StopSoundPacket stopSoundPacket = new StopSoundPacket();
+            stopSoundPacket.setSoundName(identifier);
+            session.getUpstream().sendPacket(stopSoundPacket);
+        }
+    }
+
+}
