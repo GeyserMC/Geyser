2d53425505356bb3be900cdd17f22ac606746008
DoctorMacc <toy.fighter1@gmail.com>
2020-03-31 12:21:08 -0400

    Rename BedrockPlayerInputPacket.java to BedrockPlayerInputTranslator.java

---
+++ connector/src/main/java/org/geysermc/connector/network/translators/bedrock/BedrockPlayerInputTranslator.java
@@ -0,0 +1,19 @@
+package org.geysermc.connector.network.translators.bedrock;
+
+import com.github.steveice10.mc.protocol.packet.ingame.client.world.ClientSteerVehiclePacket;
+import com.nukkitx.protocol.bedrock.packet.PlayerInputPacket;
+import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.network.translators.PacketTranslator;
+
+// Makes minecarts respond to player input
+public class BedrockPlayerInputTranslator extends PacketTranslator<PlayerInputPacket> {
+
+    @Override
+    public void translate(PlayerInputPacket packet, GeyserSession session) {
+        ClientSteerVehiclePacket clientSteerVehiclePacket = new ClientSteerVehiclePacket(
+                packet.getInputMotion().getX(), packet.getInputMotion().getY(), packet.isJumping(), packet.isSneaking()
+        );
+
+        session.getDownstream().getSession().send(clientSteerVehiclePacket);
+    }
+}
