e77a6aff026018c7c6ed4fd6cf109f5c6ae148ed
DoctorMacc <toy.fighter1@gmail.com>
2020-03-31 22:56:07 -0400

    Start on sound effects handled in this file

--- connector/src/main/java/org/geysermc/connector/network/translators/java/world/JavaServerPlayEffectTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/world/JavaServerPlayEffectTranslator.java
@@ -2,13 +2,10 @@ package org.geysermc.connector.network.translators.java.world;
 
 import com.github.steveice10.mc.protocol.data.game.world.effect.ParticleEffect;
 import com.github.steveice10.mc.protocol.data.game.world.effect.*;
-import com.github.steveice10.mc.protocol.data.game.world.effect.WorldEffect;
-import com.github.steveice10.mc.protocol.data.game.world.particle.BlockParticleData;
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerPlayEffectPacket;
 import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.protocol.bedrock.data.LevelEventType;
 import com.nukkitx.protocol.bedrock.packet.LevelEventPacket;
-import com.nukkitx.protocol.bedrock.packet.SpawnParticleEffectPacket;
 import org.geysermc.connector.GeyserConnector;
 import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.PacketTranslator;
@@ -23,43 +20,56 @@ public class JavaServerPlayEffectTranslator extends PacketTranslator<ServerPlayE
         System.out.println("Translating: " + packet.getEffect());
         System.out.println("Packet type: " + packet.getEffect().getClass());
         System.out.println("Data: " + packet.getData());
-        LevelEventPacket particle = new LevelEventPacket();
+        LevelEventPacket effect = new LevelEventPacket();
         // Some things here are particles, others are not
         if (packet.getEffect() instanceof ParticleEffect) {
             ParticleEffect particleEffect = (ParticleEffect) packet.getEffect();
             switch (particleEffect) {
                 case BONEMEAL_GROW:
-                    particle.setType(LevelEventType.BONEMEAL);
+                    effect.setType(LevelEventType.BONEMEAL);
                     BonemealGrowEffectData growEffectData = (BonemealGrowEffectData) packet.getData();
-                    particle.setData(growEffectData.getParticleCount());
+                    effect.setData(growEffectData.getParticleCount());
                     break;
                 case BREAK_BLOCK:
-                    particle.setType(LevelEventType.DESTROY);
+                    effect.setType(LevelEventType.DESTROY);
                     BreakBlockEffectData breakBlockEffectData = (BreakBlockEffectData) packet.getData();
-                    particle.setData(BlockTranslator.getBedrockBlockId(breakBlockEffectData.getBlockState()));
+                    effect.setData(BlockTranslator.getBedrockBlockId(breakBlockEffectData.getBlockState()));
                     break;
                 case BREAK_EYE_OF_ENDER:
-                    particle.setType(LevelEventType.EYE_DESPAWN);
+                    effect.setType(LevelEventType.EYE_DESPAWN);
                     break;
                 case BREAK_SPLASH_POTION:
                     // TODO: Check this one especially
-                    particle.setType(LevelEventType.SPLASH);
+                    effect.setType(LevelEventType.SPLASH);
                     break;
                 case EXPLOSION:
-                    particle.setType(LevelEventType.PARTICLE_EXPLODE);
+                    effect.setType(LevelEventType.PARTICLE_EXPLODE);
                     break;
                 case MOB_SPAWN:
-                    particle.setType(LevelEventType.ENTITY_SPAWN);
+                    effect.setType(LevelEventType.ENTITY_SPAWN);
                     break;
                 case SMOKE:
-                    particle.setType(LevelEventType.PARTICLE_SMOKE);
+                    effect.setType(LevelEventType.PARTICLE_SMOKE);
                     break;
                 default:
                     GeyserConnector.getInstance().getLogger().debug("No effect handling for effect: " + packet.getEffect());
             }
 
-            particle.setPosition(Vector3f.from(packet.getPosition().getX(), packet.getPosition().getY(), packet.getPosition().getZ()));
-            session.getUpstream().sendPacket(particle);
+            effect.setPosition(Vector3f.from(packet.getPosition().getX(), packet.getPosition().getY(), packet.getPosition().getZ()));
+            session.getUpstream().sendPacket(effect);
+        } else if (packet.getEffect() instanceof SoundEffect) {
+            SoundEffect soundEffect = (SoundEffect) packet.getEffect();
+            switch (soundEffect) {
+                // TODO: Finish these
+                // Also consider: json map for this??
+                case BLOCK_ANVIL_LAND:
+                    effect.setType(LevelEventType.SOUND_ANVIL_FALL);
+                case BLOCK_ANVIL_USE:
+                    effect.setType(LevelEventType.SOUND_ANVIL_USE);
+                    break;
+            }
+            effect.setPosition(Vector3f.from(packet.getPosition().getX(), packet.getPosition().getY(), packet.getPosition().getZ()));
+            session.getUpstream().sendPacket(effect);
         }
     }
 }
