f4fa5fdecbd3e18d18f8375103715d4e11aa625f
Brendan Grieve <brendan.grieve@zepli.com.au>
2020-04-04 17:22:35 +0800

    Move replacement to within GeyserSession::authenticate

--- connector/src/main/java/org/geysermc/connector/network/session/GeyserSession.java
+++ connector/src/main/java/org/geysermc/connector/network/session/GeyserSession.java
@@ -199,9 +199,9 @@ public class GeyserSession implements CommandSender {
             try {
                 MinecraftProtocol protocol;
                 if (password != null && !password.isEmpty()) {
-                    protocol = new MinecraftProtocol(username, password);
+                    protocol = new MinecraftProtocol(username.replace(" ","_"), password);
                 } else {
-                    protocol = new MinecraftProtocol(username);
+                    protocol = new MinecraftProtocol(username.replace(" ","_"));
                 }
 
                 boolean floodgate = connector.getAuthType() == AuthType.FLOODGATE;
