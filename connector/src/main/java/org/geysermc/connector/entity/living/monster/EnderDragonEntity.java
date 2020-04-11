13bee6ca6a0b2a6db75957cb9e4aabb9bba73112
DoctorMacc <toy.fighter1@gmail.com>
2020-04-11 13:10:35 -0400

    Debug stuff

--- connector/src/main/java/org/geysermc/connector/entity/living/monster/EnderDragonEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/living/monster/EnderDragonEntity.java
@@ -1,12 +1,20 @@
 package org.geysermc.connector.entity.living.monster;
 
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.nukkitx.math.vector.Vector3f;
 import org.geysermc.connector.entity.living.InsentientEntity;
 import org.geysermc.connector.entity.type.EntityType;
+import org.geysermc.connector.network.session.GeyserSession;
 
 public class EnderDragonEntity extends InsentientEntity {
 
     public EnderDragonEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
+    }
+
+    @Override
+    public void updateBedrockMetadata(EntityMetadata entityMetadata, GeyserSession session) {
+        System.out.println("Ender Dragon ID: " + entityMetadata.getId() + " Value: " + entityMetadata.getValue());
+        super.updateBedrockMetadata(entityMetadata, session);
     }
 }
