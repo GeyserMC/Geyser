9480398e4796d0f1c1a7d9a28d26642925f2ec72
Marco <mneuhaus44@gmail.com>
2020-04-27 21:12:35 +0200

    add licence remove debugs

--- connector/src/main/java/org/geysermc/connector/network/translators/item/translators/nbt/BasicItemTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/item/translators/nbt/BasicItemTranslator.java
@@ -1,19 +1,40 @@
+/*
+ * Copyright (c) 2019-2020 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/Geyser
+ */
+
 package org.geysermc.connector.network.translators.item.translators.nbt;
 
-import com.github.steveice10.mc.protocol.data.message.Message;
 import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
 import com.github.steveice10.opennbt.tag.builtin.ListTag;
 import com.github.steveice10.opennbt.tag.builtin.StringTag;
 import com.github.steveice10.opennbt.tag.builtin.Tag;
-import org.geysermc.connector.GeyserConnector;
-import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.ItemRemapper;
 import org.geysermc.connector.network.translators.NbtItemStackTranslator;
 import org.geysermc.connector.network.translators.item.ItemEntry;
 import org.geysermc.connector.utils.MessageUtils;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @ItemRemapper(priority = -1)
@@ -25,12 +46,9 @@ public class BasicItemTranslator extends NbtItemStackTranslator {
             CompoundTag displayTag = itemTag.get("display");
             if (displayTag.contains("Name")) {
                 StringTag nameTag = displayTag.get("Name");
-
                 try {
                     displayTag.put(new StringTag("Name", "§r" + MessageUtils.getBedrockMessage(nameTag.getValue())));
-                } catch (Exception ex) {
-                    GeyserConnector.getInstance().getLogger().error("Invalid Value " + nameTag.getValue());
-                }
+                } catch (Exception ex) {}
             }
 
             if (displayTag.contains("Lore")) {
@@ -38,12 +56,9 @@ public class BasicItemTranslator extends NbtItemStackTranslator {
                 List<Tag> lore = new ArrayList<>();
                 for (Tag tag : loreTag.getValue()) {
                     if (!(tag instanceof StringTag)) return;
-                    GeyserConnector.getInstance().getLogger().info("Value " + tag.getValue());
                     try {
                         lore.add(new StringTag("", "§r" + MessageUtils.getBedrockMessage((String) tag.getValue())));
-                    } catch (Exception ex) {
-                        GeyserConnector.getInstance().getLogger().error("Invalid Value " + tag.getValue() + " " + displayTag.toString(), ex);
-                    }
+                    } catch (Exception ex) {}
                 }
                 displayTag.put(new ListTag("Lore", lore));
             }
