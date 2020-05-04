129e10c9205c95fbe3e10da9426a78013c0c9b14
rtm516 <rtm516@users.noreply.github.com>
2020-05-04 20:17:25 +0100

    Added automatic git version infomation to startup (#327)
    * Added automatic git version infomation to startup
    
    * Moved to compile time replacement
    
    * Cleaned up old functions
    
    * Removed redundant variable
    
    * Added project version to version string

--- connector/src/main/java/org/geysermc/connector/GeyserConnector.java
+++ connector/src/main/java/org/geysermc/connector/GeyserConnector.java
@@ -28,9 +28,7 @@ package org.geysermc.connector;
 import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
 import com.nukkitx.protocol.bedrock.BedrockServer;
 import com.nukkitx.protocol.bedrock.v390.Bedrock_v390;
-
 import lombok.Getter;
-
 import org.geysermc.common.AuthType;
 import org.geysermc.common.PlatformType;
 import org.geysermc.connector.bootstrap.GeyserBootstrap;
@@ -59,7 +57,7 @@ public class GeyserConnector {
     public static final BedrockPacketCodec BEDROCK_PACKET_CODEC = Bedrock_v390.V390_CODEC;
 
     public static final String NAME = "Geyser";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "DEV"; // A fallback for running in IDEs
 
     private final Map<InetSocketAddress, GeyserSession> players = new HashMap<>();
 
