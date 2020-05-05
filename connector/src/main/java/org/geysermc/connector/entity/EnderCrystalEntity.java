GeyserMC/Geyser

commit 6642f1ec265379e6755638a043c1a11ca3e72954

Fix a few entities not showing up and include identifiers for entities in EntityType

connector/src/main/java/org/geysermc/connector/entity/EnderCrystalEntity.java
@@ -60,23 +60,4 @@ public class EnderCrystalEntity extends Entity {
         }
         super.updateBedrockMetadata(entityMetadata, session);
     }
-
-    @Override
-    public void spawnEntity(GeyserSession session) {
-        AddEntityPacket addEntityPacket = new AddEntityPacket();
-        // Not end crystal but ender crystal
-        addEntityPacket.setIdentifier("minecraft:ender_crystal");
-        addEntityPacket.setRuntimeEntityId(geyserId);
-        addEntityPacket.setUniqueEntityId(geyserId);
-        addEntityPacket.setPosition(position);
-        addEntityPacket.setMotion(motion);
-        addEntityPacket.setRotation(getBedrockRotation());
-        addEntityPacket.setEntityType(entityType.getType());
-        addEntityPacket.getMetadata().putAll(metadata);
-
-        valid = true;
-        session.getUpstream().sendPacket(addEntityPacket);
-
-        session.getConnector().getLogger().debug("Spawned entity " + entityType + " at location " + position + " with id " + geyserId + " (java id " + entityId + ")");
-    }
 }

connector/src/main/java/org/geysermc/connector/entity/ExpOrbEntity.java
@@ -26,9 +26,13 @@
 package org.geysermc.connector.entity;
 
 import com.nukkitx.math.vector.Vector3f;
-import com.nukkitx.protocol.bedrock.packet.SpawnExperienceOrbPacket;
+import com.nukkitx.protocol.bedrock.data.EntityData;
+import com.nukkitx.protocol.bedrock.packet.SetEntityMotionPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class ExpOrbEntity extends Entity {
 
@@ -42,11 +46,7 @@ public class ExpOrbEntity extends Entity {
 
     @Override
     public void spawnEntity(GeyserSession session) {
-        SpawnExperienceOrbPacket spawnExpOrbPacket = new SpawnExperienceOrbPacket();
-        spawnExpOrbPacket.setPosition(position);
-        spawnExpOrbPacket.setAmount(amount);
-
-        valid = true;
-        session.getUpstream().sendPacket(spawnExpOrbPacket);
+        this.metadata.put(EntityData.EXPERIENCE_VALUE, amount);
+        super.spawnEntity(session);
     }
 }

connector/src/main/java/org/geysermc/connector/entity/FishingHookEntity.java
@@ -34,23 +34,4 @@ public class FishingHookEntity extends Entity {
     public FishingHookEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
     }
-
-    @Override
-    public void spawnEntity(GeyserSession session) {
-        AddEntityPacket addEntityPacket = new AddEntityPacket();
-        // Different ID in Bedrock
-        addEntityPacket.setIdentifier("minecraft:fishing_hook");
-        addEntityPacket.setRuntimeEntityId(geyserId);
-        addEntityPacket.setUniqueEntityId(geyserId);
-        addEntityPacket.setPosition(position);
-        addEntityPacket.setMotion(motion);
-        addEntityPacket.setRotation(getBedrockRotation());
-        addEntityPacket.setEntityType(entityType.getType());
-        addEntityPacket.getMetadata().putAll(metadata);
-
-        valid = true;
-        session.getUpstream().sendPacket(addEntityPacket);
-
-        session.getConnector().getLogger().debug("Spawned entity " + entityType + " at location " + position + " with id " + geyserId + " (java id " + entityId + ")");
-    }
 }

connector/src/main/java/org/geysermc/connector/entity/living/animal/TropicalFishEntity.java
@@ -54,24 +54,6 @@ public class TropicalFishEntity extends AbstractFishEntity {
         super.updateBedrockMetadata(entityMetadata, session);
     }
 
-    @Override
-    public void spawnEntity(GeyserSession session) {
-        AddEntityPacket addEntityPacket = new AddEntityPacket();
-        addEntityPacket.setIdentifier("minecraft:tropicalfish");
-        addEntityPacket.setRuntimeEntityId(geyserId);
-        addEntityPacket.setUniqueEntityId(geyserId);
-        addEntityPacket.setPosition(position);
-        addEntityPacket.setMotion(motion);
-        addEntityPacket.setRotation(getBedrockRotation());
-        addEntityPacket.setEntityType(entityType.getType());
-        addEntityPacket.getMetadata().putAll(metadata);
-
-        valid = true;
-        session.getUpstream().sendPacket(addEntityPacket);
-
-        session.getConnector().getLogger().debug("Spawned entity " + entityType + " at location " + position + " with id " + geyserId + " (java id " + entityId + ")");
-    }
-
     @Getter
     @AllArgsConstructor
     private static class TropicalFishVariant {

connector/src/main/java/org/geysermc/connector/entity/living/animal/horse/TraderLlamaEntity.java
@@ -26,36 +26,11 @@
 package org.geysermc.connector.entity.living.animal.horse;
 
 import com.nukkitx.math.vector.Vector3f;
-import com.nukkitx.protocol.bedrock.data.EntityData;
-import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
 import org.geysermc.connector.entity.type.EntityType;
-import org.geysermc.connector.network.session.GeyserSession;
 
 public class TraderLlamaEntity extends LlamaEntity {
 
     public TraderLlamaEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
     }
-
-    @Override
-    public void spawnEntity(GeyserSession session) {
-        // The trader llama is a separate entity from the llama in Java but a normal llama with extra metadata in Bedrock.
-        AddEntityPacket addEntityPacket = new AddEntityPacket();
-        addEntityPacket.setIdentifier("minecraft:llama");
-        addEntityPacket.setRuntimeEntityId(geyserId);
-        addEntityPacket.setUniqueEntityId(geyserId);
-        addEntityPacket.setPosition(position);
-        addEntityPacket.setMotion(motion);
-        addEntityPacket.setRotation(getBedrockRotation());
-        addEntityPacket.setEntityType(entityType.getType());
-        addEntityPacket.getMetadata().putAll(metadata);
-        // Here's the difference
-        addEntityPacket.getMetadata().put(EntityData.MARK_VARIANT, 1);
-
-        valid = true;
-        session.getUpstream().sendPacket(addEntityPacket);
-
-        session.getConnector().getLogger().debug("Spawned entity " + entityType + " at location " + position + " with id " + geyserId + " (java id " + entityId + ")");
-    }
-
 }

connector/src/main/java/org/geysermc/connector/entity/Entity.java
@@ -109,7 +109,7 @@ public class Entity {
 
     public void spawnEntity(GeyserSession session) {
         AddEntityPacket addEntityPacket = new AddEntityPacket();
-        addEntityPacket.setIdentifier("minecraft:" + entityType.name().toLowerCase());
+        addEntityPacket.setIdentifier(entityType.getIdentifier());
         addEntityPacket.setRuntimeEntityId(geyserId);
         addEntityPacket.setUniqueEntityId(geyserId);
         addEntityPacket.setPosition(position);

connector/src/main/java/org/geysermc/connector/entity/living/merchant/VillagerEntity.java
@@ -29,7 +29,6 @@ import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadat
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.VillagerData;
 import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.protocol.bedrock.data.EntityData;
-import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import org.geysermc.connector.entity.type.EntityType;
@@ -85,24 +84,4 @@ public class VillagerEntity extends AbstractMerchantEntity {
         }
         super.updateBedrockMetadata(entityMetadata, session);
     }
-
-    @Override
-    public void spawnEntity(GeyserSession session) {
-        AddEntityPacket addEntityPacket = new AddEntityPacket();
-        // "v2" or else it's the legacy villager
-        addEntityPacket.setIdentifier("minecraft:villager_v2");
-        addEntityPacket.setRuntimeEntityId(geyserId);
-        addEntityPacket.setUniqueEntityId(geyserId);
-        addEntityPacket.setPosition(position);
-        addEntityPacket.setMotion(motion);
-        addEntityPacket.setRotation(getBedrockRotation());
-        addEntityPacket.setEntityType(entityType.getType());
-        addEntityPacket.getMetadata().putAll(metadata);
-
-        valid = true;
-        session.getUpstream().sendPacket(addEntityPacket);
-
-        session.getConnector().getLogger().debug("Spawned entity " + entityType + " at location " + position + " with id " + geyserId + " (java id " + entityId + ")");
-    }
-
 }

connector/src/main/java/org/geysermc/connector/entity/type/EntityType.java
@@ -45,7 +45,7 @@ public enum EntityType {
     PIG(PigEntity.class, 12, 0.9f),
     SHEEP(SheepEntity.class, 13, 1.3f, 0.9f),
     WOLF(WolfEntity.class, 14, 0.85f, 0.6f),
-    VILLAGER(VillagerEntity.class, 15, 1.8f, 0.6f, 0.6f, 1.62f),
+    VILLAGER(VillagerEntity.class, 15, 1.8f, 0.6f, 0.6f, 1.62f, "minecraft:villager_v2"),
     MOOSHROOM(AnimalEntity.class, 16, 1.4f, 0.9f),
     SQUID(WaterEntity.class, 17, 0.8f),
     RABBIT(RabbitEntity.class, 18, 0.5f, 0.4f),
@@ -60,7 +60,7 @@ public enum EntityType {
     ZOMBIE_HORSE(AbstractHorseEntity.class, 27, 1.6f, 1.3965f),
     POLAR_BEAR(PolarBearEntity.class, 28, 1.4f, 1.3f),
     LLAMA(LlamaEntity.class, 29, 1.87f, 0.9f),
-    TRADER_LLAMA(TraderLlamaEntity.class, 29, 1.187f, 0.9f),
+    TRADER_LLAMA(TraderLlamaEntity.class, 29, 1.187f, 0.9f, 0f, 0f, "minecraft:llama"),
     PARROT(ParrotEntity.class, 30, 0.9f, 0.5f),
     DOLPHIN(WaterEntity.class, 31, 0.6f, 0.9f),
     ZOMBIE(ZombieEntity.class, 32, 1.8f, 0.6f, 0.6f, 1.62f),
@@ -101,16 +101,16 @@ public enum EntityType {
     TNT(TNTEntity.class, 65, 0.98f, 0.98f),
     FALLING_BLOCK(FallingBlockEntity.class, 66, 0.98f, 0.98f),
     MOVING_BLOCK(Entity.class, 67, 0f),
-    EXPERIENCE_BOTTLE(ThrowableEntity.class, 68, 0.25f, 0.25f),
-    EXPERIENCE_ORB(ExpOrbEntity.class, 69, 0f),
+    EXPERIENCE_BOTTLE(ThrowableEntity.class, 68, 0.25f, 0.25f, 0f, 0f, "minecraft:xp_bottle"),
+    EXPERIENCE_ORB(ExpOrbEntity.class, 69, 0f, 0f, 0f, 0f, "minecraft:xp_orb"),
     EYE_OF_ENDER(Entity.class, 70, 0f),
-    END_CRYSTAL(EnderCrystalEntity.class, 71, 0f),
+    END_CRYSTAL(EnderCrystalEntity.class, 71, 0f, 0f, 0f, 0f, "minecraft:ender_crystal"),
     FIREWORK_ROCKET(Entity.class, 72, 0f),
     TRIDENT(ArrowEntity.class, 73, 0f),
     TURTLE(AnimalEntity.class, 74, 0.4f, 1.2f),
     CAT(CatEntity.class, 75, 0.35f, 0.3f),
     SHULKER_BULLET(Entity.class, 76, 0f),
-    FISHING_BOBBER(FishingHookEntity.class, 77, 0f),
+    FISHING_BOBBER(FishingHookEntity.class, 77, 0f, 0f, 0f, 0f, "minecraft:fishing_hook"),
     CHALKBOARD(Entity.class, 78, 0f),
     DRAGON_FIREBALL(ItemedFireballEntity.class, 79, 0f),
     ARROW(ArrowEntity.class, 80, 0.25f, 0.25f),
@@ -143,7 +143,7 @@ public enum EntityType {
     PUFFERFISH(PufferFishEntity.class, 108, 0.7f, 0.7f),
     SALMON(AbstractFishEntity.class, 109, 0.5f, 0.7f),
     DROWNED(ZombieEntity.class, 110, 1.95f, 0.6f),
-    TROPICAL_FISH(TropicalFishEntity.class, 111, 0.6f, 0.6f),
+    TROPICAL_FISH(TropicalFishEntity.class, 111, 0.6f, 0.6f, 0f, 0f, "minecraft:tropicalfish"),
     COD(AbstractFishEntity.class, 112, 0.25f, 0.5f),
     PANDA(PandaEntity.class, 113, 1.25f, 1.125f, 1.825f),
     FOX(FoxEntity.class, 121, 0.5f, 1.25f),
@@ -160,6 +160,7 @@ public enum EntityType {
     private final float width;
     private final float length;
     private final float offset;
+    private String identifier;
 
     EntityType(Class<? extends Entity> entityClass, int type, float height) {
         this(entityClass, type, height, 0f);
@@ -174,11 +175,18 @@ public enum EntityType {
     }
 
     EntityType(Class<? extends Entity> entityClass, int type, float height, float width, float length, float offset) {
+        this(entityClass, type, height, width, length, offset, null);
+
+        this.identifier = "minecraft:" + name().toLowerCase();
+    }
+
+    EntityType(Class<? extends Entity> entityClass, int type, float height, float width, float length, float offset, String identifier) {
         this.entityClass = entityClass;
         this.type = type;
         this.height = height;
         this.width = width;
         this.length = length;
         this.offset = offset + 0.00001f;
+        this.identifier = identifier;
     }
 }

commit 96a7770c22cfc1db21cc2f840f511e543177710a

Fixed entity bugs introduced by 6642f1e and added fishing lines (#483)
* Fixed entity bugs introduced by 6642f1e

* Fixed fishing line not displaying

* Clean extra line and added todo

connector/src/main/java/org/geysermc/connector/entity/EnderCrystalEntity.java
@@ -31,7 +31,6 @@ import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.math.vector.Vector3i;
 import com.nukkitx.protocol.bedrock.data.EntityData;
 import com.nukkitx.protocol.bedrock.data.EntityFlag;
-import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
 

connector/src/main/java/org/geysermc/connector/entity/ExpOrbEntity.java
@@ -27,12 +27,8 @@ package org.geysermc.connector.entity;
 
 import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.protocol.bedrock.data.EntityData;
-import com.nukkitx.protocol.bedrock.packet.SetEntityMotionPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
-
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 public class ExpOrbEntity extends Entity {
 

connector/src/main/java/org/geysermc/connector/entity/FishingHookEntity.java
@@ -25,13 +25,23 @@
 
 package org.geysermc.connector.entity;
 
+import com.github.steveice10.mc.protocol.data.game.entity.type.object.ProjectileData;
 import com.nukkitx.math.vector.Vector3f;
-import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
+import com.nukkitx.protocol.bedrock.data.EntityData;
+import org.geysermc.connector.GeyserConnector;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
 
 public class FishingHookEntity extends Entity {
-    public FishingHookEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
+    public FishingHookEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation, ProjectileData data) {
         super(entityId, geyserId, entityType, position, motion, rotation);
+
+        // TODO: Find a better way to do this
+        for (GeyserSession session : GeyserConnector.getInstance().getPlayers().values()) {
+            if (session.getPlayerEntity().getEntityId() == data.getOwnerId()) {
+                this.metadata.put(EntityData.OWNER_EID, session.getPlayerEntity().getGeyserId());
+                return;
+            }
+        }
     }
 }

connector/src/main/java/org/geysermc/connector/entity/living/animal/TropicalFishEntity.java
@@ -28,7 +28,6 @@ package org.geysermc.connector.entity.living.animal;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.protocol.bedrock.data.EntityData;
-import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.geysermc.connector.entity.living.AbstractFishEntity;

connector/src/main/java/org/geysermc/connector/entity/living/animal/horse/TraderLlamaEntity.java
@@ -26,11 +26,19 @@
 package org.geysermc.connector.entity.living.animal.horse;
 
 import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.protocol.bedrock.data.EntityData;
 import org.geysermc.connector.entity.type.EntityType;
+import org.geysermc.connector.network.session.GeyserSession;
 
 public class TraderLlamaEntity extends LlamaEntity {
 
     public TraderLlamaEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
+    }
+
+    @Override
+    public void spawnEntity(GeyserSession session) {
+        this.metadata.put(EntityData.MARK_VARIANT, 1);
+        super.spawnEntity(session);
     }
 }

connector/src/main/java/org/geysermc/connector/network/translators/java/entity/spawn/JavaSpawnObjectTranslator.java
@@ -25,13 +25,15 @@
 
 package org.geysermc.connector.network.translators.java.entity.spawn;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 import com.github.steveice10.mc.protocol.data.game.entity.type.object.FallingBlockData;
 import com.github.steveice10.mc.protocol.data.game.entity.type.object.HangingDirection;
+import com.github.steveice10.mc.protocol.data.game.entity.type.object.ObjectType;
+import com.github.steveice10.mc.protocol.data.game.entity.type.object.ProjectileData;
+import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnObjectPacket;
+import com.nukkitx.math.vector.Vector3f;
 import org.geysermc.connector.entity.Entity;
 import org.geysermc.connector.entity.FallingBlockEntity;
+import org.geysermc.connector.entity.FishingHookEntity;
 import org.geysermc.connector.entity.ItemFrameEntity;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
@@ -39,9 +41,8 @@ import org.geysermc.connector.network.translators.PacketTranslator;
 import org.geysermc.connector.network.translators.Translator;
 import org.geysermc.connector.utils.EntityUtils;
 
-import com.github.steveice10.mc.protocol.data.game.entity.type.object.ObjectType;
-import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnObjectPacket;
-import com.nukkitx.math.vector.Vector3f;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 @Translator(packet = ServerSpawnObjectPacket.class)
 public class JavaSpawnObjectTranslator extends PacketTranslator<ServerSpawnObjectPacket> {
@@ -69,6 +70,10 @@ public class JavaSpawnObjectTranslator extends PacketTranslator<ServerSpawnObjec
                 // Item frames need the hanging direction
                 entity = new ItemFrameEntity(packet.getEntityId(), session.getEntityCache().getNextEntityId().incrementAndGet(),
                         type, position, motion, rotation, (HangingDirection) packet.getData());
+            } else if (packet.getType() == ObjectType.FISHING_BOBBER) {
+                // Fishing bobbers need the owner for the line
+                entity = new FishingHookEntity(packet.getEntityId(), session.getEntityCache().getNextEntityId().incrementAndGet(),
+                        type, position, motion, rotation, (ProjectileData) packet.getData());
             } else {
                 Constructor<? extends Entity> entityConstructor = entityClass.getConstructor(long.class, long.class, EntityType.class,
                         Vector3f.class, Vector3f.class, Vector3f.class);