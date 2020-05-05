6c9af4237582beb37680f380d9b86f89ea7bbf4f
rtm516 <ryantmilner@hotmail.co.uk>
2020-05-05 11:12:14 +0100

    Fixed entity bugs introduced by 6642f1e

--- connector/src/main/java/org/geysermc/connector/entity/living/animal/horse/TraderLlamaEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/living/animal/horse/TraderLlamaEntity.java
@@ -26,11 +26,19 @@
 package org.geysermc.connector.entity.living.animal.horse;
 
 import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.protocol.bedrock.data.EntityData;
 import org.geysermc.connector.entity.type.EntityType;
+import org.geysermc.connector.network.session.GeyserSession;
 
 public class TraderLlamaEntity extends LlamaEntity {
 
     public TraderLlamaEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
+    }
+
+    @Override
+    public void spawnEntity(GeyserSession session) {
+        this.metadata.put(EntityData.MARK_VARIANT, 1);
+        super.spawnEntity(session);
     }
 }
