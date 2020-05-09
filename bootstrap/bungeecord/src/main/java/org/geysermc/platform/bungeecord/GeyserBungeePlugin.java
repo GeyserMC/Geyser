7b3893ff78d4e484386436e361f0d545a967997d
rtm516 <rtm516@users.noreply.github.com>
2020-05-09 04:58:29 +0100

    Fixed empty listen IPs breaking automatic config (#519)

--- bootstrap/bungeecord/src/main/java/org/geysermc/platform/bungeecord/GeyserBungeePlugin.java
+++ bootstrap/bungeecord/src/main/java/org/geysermc/platform/bungeecord/GeyserBungeePlugin.java
@@ -91,7 +91,7 @@ public class GeyserBungeePlugin extends Plugin implements GeyserBootstrap {
 
             // Don't change the ip if its listening on all interfaces
             // By default this should be 127.0.0.1 but may need to be changed in some circumstances
-            if (!javaAddr.getHostString().equals("0.0.0.0")) {
+            if (!javaAddr.getHostString().equals("0.0.0.0") && !javaAddr.getHostString().equals("")) {
                 configuration.set("remote.address", javaAddr.getHostString());
             }
 
