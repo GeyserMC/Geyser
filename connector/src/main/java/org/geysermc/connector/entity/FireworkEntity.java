825de5d404896fb89c5313062534227119ba18a9
OnlyBMan <27742182+OnlyBMan@users.noreply.github.com>
2020-05-14 14:31:02 -0400

    Change how to detect player glide

--- connector/src/main/java/org/geysermc/connector/entity/FireworkEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/FireworkEntity.java
@@ -28,11 +28,11 @@ package org.geysermc.connector.entity;
 
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.nukkitx.math.vector.Vector3f;
-import com.nukkitx.protocol.bedrock.data.EntityFlag;
 import com.nukkitx.protocol.bedrock.packet.SetEntityMotionPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
-import org.geysermc.connector.network.translators.item.ItemTranslator;
+
+import java.util.OptionalInt;
 
 public class FireworkEntity extends Entity {
 
@@ -42,18 +42,19 @@ public class FireworkEntity extends Entity {
 
     @Override
     public void updateBedrockMetadata(EntityMetadata entityMetadata, GeyserSession session) {
-        if (session.getInventory().getItem(session.getInventory().getHeldItemSlot() + 36).getId() == ItemTranslator.FIREWORK_ROCKET && session.getPlayerEntity().getMetadata().getFlags().getFlag(EntityFlag.GLIDING)) {
-            float yaw = session.getPlayerEntity().getRotation().getX();
-            float pitch = session.getPlayerEntity().getRotation().getY();
+        if (entityMetadata.getId() == 8 && !entityMetadata.getValue().equals(OptionalInt.empty()) && ((OptionalInt) entityMetadata.getValue()).getAsInt() == session.getPlayerEntity().getEntityId()) {
+            PlayerEntity entity = session.getPlayerEntity();
+            float yaw = entity.getRotation().getX();
+            float pitch = entity.getRotation().getY();
             //Uses math from NukkitX
-            session.getPlayerEntity().setMotion(Vector3f.from(
+            entity.setMotion(Vector3f.from(
                     -Math.sin(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch)) * 2,
                     -Math.sin(Math.toRadians(pitch)) * 2,
                     Math.cos(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch)) * 2));
 
             SetEntityMotionPacket entityMotionPacket = new SetEntityMotionPacket();
-            entityMotionPacket.setRuntimeEntityId(session.getPlayerEntity().getGeyserId());
-            entityMotionPacket.setMotion(session.getPlayerEntity().getMotion());
+            entityMotionPacket.setRuntimeEntityId(entity.getGeyserId());
+            entityMotionPacket.setMotion(entity.getMotion());
 
             session.sendUpstreamPacket(entityMotionPacket);
         }
