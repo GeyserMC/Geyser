18415d5d15dab8e20bbf45176b10fcf5e5fb26dd
rtm516 <rtm516@users.noreply.github.com>
2020-05-20 18:39:58 +0100

    Fixed trident display and some arrow related cleanup (#609)
    * Fixed trident display and some arrow related cleanup
    
    * Renamed trident entity

--- connector/src/main/java/org/geysermc/connector/entity/TridentEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/TridentEntity.java
@@ -25,12 +25,24 @@
 
 package org.geysermc.connector.entity;
 
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.protocol.bedrock.data.EntityFlag;
 import org.geysermc.connector.entity.type.EntityType;
+import org.geysermc.connector.network.session.GeyserSession;
 
-public class ArrowEntity extends Entity {
+public class TridentEntity extends AbstractArrowEntity {
 
-    public ArrowEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
+    public TridentEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
+    }
+
+    @Override
+    public void updateBedrockMetadata(EntityMetadata entityMetadata, GeyserSession session) {
+        if (entityMetadata.getId() == 11) {
+            metadata.getFlags().setFlag(EntityFlag.ENCHANTED, (boolean) entityMetadata.getValue());
+        }
+
+        super.updateBedrockMetadata(entityMetadata, session);
     }
 }
