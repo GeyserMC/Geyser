0734f8c4c267d622c41080874218d23a53db575f
bundabrg <brendan@grieve.com.au>
2020-05-26 15:37:31 +0800

    Implement Block Mapping Overrides
    * All attempts to load a resource will first look for the path under a "resources" folder relative to its folder. This allows overriding any resource at runtime.
    * When loading mappings/blocks.json we also look for a overrides/blocks.json. Any entry in the override file will override the entry in the mappings file allowing one to selectively override blocks.

--- connector/src/main/java/org/geysermc/connector/network/translators/world/block/BlockTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/world/block/BlockTranslator.java
@@ -42,6 +42,7 @@ import org.geysermc.connector.network.translators.world.block.entity.BlockEntity
 import org.geysermc.connector.utils.FileUtils;
 import org.reflections.Reflections;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
@@ -97,6 +98,13 @@ public class BlockTranslator {
         } catch (Exception e) {
             throw new AssertionError("Unable to load Java block mappings", e);
         }
+
+        // Load Block Overrides
+        JsonNode blocksOverride = null;
+        try (InputStream is = FileUtils.getResource("overrides/blocks.json")) {
+            blocksOverride = GeyserConnector.JSON_MAPPER.readTree(is);
+        } catch (IOException | AssertionError ignored) { }
+
         Object2IntMap<CompoundTag> addedStatesMap = new Object2IntOpenHashMap<>();
         addedStatesMap.defaultReturnValue(-1);
         List<CompoundTag> paletteList = new ArrayList<>();
@@ -113,6 +121,12 @@ public class BlockTranslator {
             javaRuntimeId++;
             Map.Entry<String, JsonNode> entry = blocksIterator.next();
             String javaId = entry.getKey();
+
+            // Check for an override
+            if (blocksOverride != null && blocksOverride.has(javaId)) {
+                entry = new AbstractMap.SimpleEntry<>(javaId, blocksOverride.get(javaId));
+            }
+
             BlockState javaBlockState = new BlockState(javaRuntimeId);
             CompoundTag blockTag = buildBedrockState(entry.getValue());
 
