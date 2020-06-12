19f6ec51233d7dad83a9c901dfbee313fe97636d
Brendan Grieve <brendan.grieve@zepli.com.au>
2020-06-12 15:12:36 +0800

    Merge Fixes

--- connector/src/main/java/org/geysermc/connector/configuration/GeyserJacksonConfiguration.java
+++ connector/src/main/java/org/geysermc/connector/configuration/GeyserJacksonConfiguration.java
@@ -84,6 +84,9 @@ public abstract class GeyserJacksonConfiguration implements GeyserConfiguration 
     @JsonProperty("cache-chunks")
     private boolean cacheChunks;
 
+    @JsonProperty("cache-skins")
+    private int cacheSkins = 180;
+
     @JsonProperty("above-bedrock-nether-building")
     private boolean aboveBedrockNetherBuilding;
 
