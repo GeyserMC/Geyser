6aaaee3d380ff8ec1a0abe225f8ffcc1cdc0141d
bundabrg <brendan@grieve.com.au>
2020-06-29 10:27:24 +0800

    Use MessageUtils to translate Message to String

--- connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerListEntryTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/java/entity/player/JavaPlayerListEntryTranslator.java
@@ -30,6 +30,7 @@ import org.geysermc.connector.entity.PlayerEntity;
 import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.PacketTranslator;
 import org.geysermc.connector.network.translators.Translator;
+import org.geysermc.connector.utils.MessageUtils;
 import org.geysermc.connector.utils.SkinUtils;
 
 import com.github.steveice10.mc.protocol.data.game.PlayerListEntry;
@@ -81,7 +82,7 @@ public class JavaPlayerListEntryTranslator extends PacketTranslator<ServerPlayer
                     playerEntity.setProfile(entry.getProfile());
                     playerEntity.setPlayerList(true);
                     playerEntity.setValid(true);
-                    playerEntity.setDisplayName(entry.getDisplayName() != null ? entry.getDisplayName().getFullText() : null);
+                    playerEntity.setDisplayName(entry.getDisplayName() != null ? MessageUtils.getBedrockMessage(entry.getDisplayName()) : null);
 
                     PlayerListPacket.Entry playerListEntry = SkinUtils.buildCachedEntry(playerEntity);
                     if (self) {
