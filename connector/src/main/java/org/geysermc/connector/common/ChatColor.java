575ca60e44f7f918847bebf48bdcabd7b01e7ff5
Marco <mneuhaus44@gmail.com>
2020-07-24 19:10:19 +0200

    add RGB support to ChatColor

--- connector/src/main/java/org/geysermc/connector/common/ChatColor.java
+++ connector/src/main/java/org/geysermc/connector/common/ChatColor.java
@@ -26,37 +26,42 @@
 
 package org.geysermc.connector.common;
 
+import java.util.regex.Pattern;
+
 public class ChatColor {
 
-    public static final char ESCAPE = '§';
-    public static final String BLACK = ESCAPE + "0";
-    public static final String DARK_BLUE = ESCAPE + "1";
-    public static final String DARK_GREEN = ESCAPE + "2";
-    public static final String DARK_AQUA = ESCAPE + "3";
-    public static final String DARK_RED = ESCAPE + "4";
-    public static final String DARK_PURPLE = ESCAPE + "5";
-    public static final String GOLD = ESCAPE + "6";
-    public static final String GRAY = ESCAPE + "7";
-    public static final String DARK_GRAY = ESCAPE + "8";
-    public static final String BLUE = ESCAPE + "9";
-    public static final String GREEN = ESCAPE + "a";
-    public static final String AQUA = ESCAPE + "b";
-    public static final String RED = ESCAPE + "c";
-    public static final String LIGHT_PURPLE = ESCAPE + "d";
-    public static final String YELLOW = ESCAPE + "e";
-    public static final String WHITE = ESCAPE + "f";
-    public static final String OBFUSCATED = ESCAPE + "k";
-    public static final String BOLD = ESCAPE + "l";
-    public static final String STRIKETHROUGH = ESCAPE + "m";
-    public static final String UNDERLINE = ESCAPE + "n";
-    public static final String ITALIC = ESCAPE + "o";
-    public static final String RESET = ESCAPE + "r";
+    public static final char COLOR_CODE = '§';
+    public static final String ALL_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRrXx";
+
+    public static final String BLACK = COLOR_CODE + "0";
+    public static final String DARK_BLUE = COLOR_CODE + "1";
+    public static final String DARK_GREEN = COLOR_CODE + "2";
+    public static final String DARK_AQUA = COLOR_CODE + "3";
+    public static final String DARK_RED = COLOR_CODE + "4";
+    public static final String DARK_PURPLE = COLOR_CODE + "5";
+    public static final String GOLD = COLOR_CODE + "6";
+    public static final String GRAY = COLOR_CODE + "7";
+    public static final String DARK_GRAY = COLOR_CODE + "8";
+    public static final String BLUE = COLOR_CODE + "9";
+    public static final String GREEN = COLOR_CODE + "a";
+    public static final String AQUA = COLOR_CODE + "b";
+    public static final String RED = COLOR_CODE + "c";
+    public static final String LIGHT_PURPLE = COLOR_CODE + "d";
+    public static final String YELLOW = COLOR_CODE + "e";
+    public static final String WHITE = COLOR_CODE + "f";
+    public static final String OBFUSCATED = COLOR_CODE + "k";
+    public static final String BOLD = COLOR_CODE + "l";
+    public static final String STRIKETHROUGH = COLOR_CODE + "m";
+    public static final String UNDERLINE = COLOR_CODE + "n";
+    public static final String ITALIC = COLOR_CODE + "o";
+    public static final String RESET = COLOR_CODE + "r";
+
+    public static final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)" + COLOR_CODE + "[0-9A-FK-ORX]");
 
     /**
      * Convert chat colour codes to terminal colours
      *
      * @param string The text to replace colours for
-     *
      * @return A string ready for terminal printing
      */
     public static String toANSI(String string) {
@@ -85,18 +90,24 @@ public class ChatColor {
         return string;
     }
 
-    public String translateAlternateColorCodes(char color, String message) {
-        return message.replace(color, ESCAPE);
+    public String translateAlternateColorCodes(char code, String message) {
+        char[] c = message.toCharArray();
+        for (int i = 0; i < c.length - 1; i++) {
+            if (c[i] == code && ALL_CODES.indexOf(c[i + 1]) > -1) {
+                c[i] = COLOR_CODE;
+            }
+        }
+        return new String(c);
     }
 
     /**
      * Remove all colour formatting tags from a message
      *
      * @param message Message to remove colour tags from
-     *
      * @return The sanitised message
      */
     public static String stripColors(String message) {
-        return message = message.replaceAll("(&([a-fk-or0-9]))","").replaceAll("(§([a-fk-or0-9]))","").replaceAll("s/\\x1b\\[[0-9;]*[a-zA-Z]//g","");
+        if (message == null) return null;
+        return STRIP_COLOR_PATTERN.matcher(message).replaceAll("");
     }
 }
