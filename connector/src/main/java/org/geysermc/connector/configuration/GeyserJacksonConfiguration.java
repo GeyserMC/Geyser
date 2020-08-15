8a18cdb9bf1b4dda11418a4a4c80d64f25a8c87e
Camotoy <20743703+DoctorMacc@users.noreply.github.com>
2020-08-15 15:56:51 -0400

    Add sensitive property to the Bedrock remote address

--- connector/src/main/java/org/geysermc/connector/configuration/GeyserJacksonConfiguration.java
+++ connector/src/main/java/org/geysermc/connector/configuration/GeyserJacksonConfiguration.java
@@ -98,6 +98,7 @@ public abstract class GeyserJacksonConfiguration implements GeyserConfiguration 
     @Getter
     public static class BedrockConfiguration implements IBedrockConfiguration {
 
+        @AsteriskSerializer.Asterisk(sensitive = true)
         private String address;
 
         @Setter
