a3330305798cae695beffa57e1fab984cb95eb80
Heath123 <heath.mitchell27@gmail.com>
2020-09-10 21:41:44 +0100

    Fix .gitignore

---
+++ connector/main/java/org/geysermc/connector/network/session/cache/BossBar.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2019-2020 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/Geyser
+ */
+
+package org.geysermc.connector.network.session.cache;
+
+import com.github.steveice10.mc.protocol.data.message.Message;
+import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.protocol.bedrock.data.entity.EntityData;
+import com.nukkitx.protocol.bedrock.packet.AddEntityPacket;
+import com.nukkitx.protocol.bedrock.packet.BossEventPacket;
+import com.nukkitx.protocol.bedrock.packet.RemoveEntityPacket;
+import lombok.AllArgsConstructor;
+import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.utils.MessageUtils;
+
+@AllArgsConstructor
+public class BossBar {
+
+    private GeyserSession session;
+
+    private long entityId;
+    private Message title;
+    private float health;
+    private int color;
+    private int overlay;
+    private int darkenSky;
+
+    public void addBossBar() {
+        addBossEntity();
+        updateBossBar();
+    }
+
+    //TODO: There is a player unique entity ID - if this didn't exist before, we may be able to get rid of our hack
+
+    public void updateBossBar() {
+        BossEventPacket bossEventPacket = new BossEventPacket();
+        bossEventPacket.setBossUniqueEntityId(entityId);
+        bossEventPacket.setAction(BossEventPacket.Action.CREATE);
+        bossEventPacket.setTitle(MessageUtils.getTranslatedBedrockMessage(title, session.getClientData().getLanguageCode()));
+        bossEventPacket.setHealthPercentage(health);
+        bossEventPacket.setColor(color); //ignored by client
+        bossEventPacket.setOverlay(overlay);
+        bossEventPacket.setDarkenSky(darkenSky);
+
+        session.sendUpstreamPacket(bossEventPacket);
+    }
+
+    public void updateTitle(Message title) {
+        this.title = title;
+        BossEventPacket bossEventPacket = new BossEventPacket();
+        bossEventPacket.setBossUniqueEntityId(entityId);
+        bossEventPacket.setAction(BossEventPacket.Action.UPDATE_NAME);
+        bossEventPacket.setTitle(MessageUtils.getTranslatedBedrockMessage(title, session.getClientData().getLanguageCode()));
+
+        session.sendUpstreamPacket(bossEventPacket);
+    }
+
+    public void updateHealth(float health) {
+        this.health = health;
+        BossEventPacket bossEventPacket = new BossEventPacket();
+        bossEventPacket.setBossUniqueEntityId(entityId);
+        bossEventPacket.setAction(BossEventPacket.Action.UPDATE_PERCENTAGE);
+        bossEventPacket.setHealthPercentage(health);
+
+        session.sendUpstreamPacket(bossEventPacket);
+    }
+
+    public void removeBossBar() {
+        BossEventPacket bossEventPacket = new BossEventPacket();
+        bossEventPacket.setBossUniqueEntityId(entityId);
+        bossEventPacket.setAction(BossEventPacket.Action.REMOVE);
+
+        session.sendUpstreamPacket(bossEventPacket);
+        removeBossEntity();
+    }
+
+    /**
+     * Bedrock still needs an entity to display the BossBar.<br>
+     * Just like 1.8 but it doesn't care about which entity
+     */
+    private void addBossEntity() {
+        AddEntityPacket addEntityPacket = new AddEntityPacket();
+        addEntityPacket.setUniqueEntityId(entityId);
+        addEntityPacket.setRuntimeEntityId(entityId);
+        addEntityPacket.setIdentifier("minecraft:creeper");
+        addEntityPacket.setEntityType(33);
+        addEntityPacket.setPosition(session.getPlayerEntity().getPosition().sub(0D, -10D, 0D));
+        addEntityPacket.setRotation(Vector3f.ZERO);
+        addEntityPacket.setMotion(Vector3f.ZERO);
+        addEntityPacket.getMetadata()
+                .putFloat(EntityData.SCALE, 0F)
+                .putFloat(EntityData.BOUNDING_BOX_WIDTH, 0F)
+                .putFloat(EntityData.BOUNDING_BOX_HEIGHT, 0F);
+
+        session.sendUpstreamPacket(addEntityPacket);
+    }
+
+    private void removeBossEntity() {
+        RemoveEntityPacket removeEntityPacket = new RemoveEntityPacket();
+        removeEntityPacket.setUniqueEntityId(entityId);
+
+        session.sendUpstreamPacket(removeEntityPacket);
+    }
+}
---
+++ connector/main/java/org/geysermc/connector/network/session/cache/ChunkCache.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2019-2020 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/Geyser
+ */
+
+package org.geysermc.connector.network.session.cache;
+
+import com.github.steveice10.mc.protocol.data.game.chunk.Chunk;
+import com.github.steveice10.mc.protocol.data.game.chunk.Column;
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.Position;
+import lombok.Getter;
+import org.geysermc.connector.bootstrap.GeyserBootstrap;
+import org.geysermc.connector.network.session.GeyserSession;
+import org.geysermc.connector.network.translators.world.block.BlockTranslator;
+import org.geysermc.connector.network.translators.world.chunk.ChunkPosition;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChunkCache {
+
+    private final boolean cache;
+
+    @Getter
+    private Map<ChunkPosition, Column> chunks = new HashMap<>();
+
+    public ChunkCache(GeyserSession session) {
+        if (session.getConnector().getWorldManager().getClass() == GeyserBootstrap.DEFAULT_CHUNK_MANAGER.getClass()) {
+            this.cache = session.getConnector().getConfig().isCacheChunks();
+        } else {
+            this.cache = false; // To prevent Spigot from initializing
+        }
+    }
+
+    public void addToCache(Column chunk) {
+        if (!cache) {
+            return;
+        }
+        ChunkPosition position = new ChunkPosition(chunk.getX(), chunk.getZ());
+        chunks.put(position, chunk);
+    }
+
+    public void updateBlock(Position position, int block) {
+        if (!cache) {
+            return;
+        }
+        ChunkPosition chunkPosition = new ChunkPosition(position.getX() >> 4, position.getZ() >> 4);
+        if (!chunks.containsKey(chunkPosition))
+            return;
+
+        Column column = chunks.get(chunkPosition);
+        Chunk chunk = column.getChunks()[position.getY() >> 4];
+        Position blockPosition = chunkPosition.getChunkBlock(position.getX(), position.getY(), position.getZ());
+        if (chunk != null) {
+            // System.out.println("Before: " + chunk.get(blockPosition.getX(), blockPosition.getY(), blockPosition.getZ()));
+            // System.out.println("Setting: " + block);
+            chunk.set(blockPosition.getX(), blockPosition.getY(), blockPosition.getZ(), block);
+            // System.out.println("After: " + chunk.get(blockPosition.getX(), blockPosition.getY(), blockPosition.getZ()));
+        }
+    }
+
+    public int getBlockAt(Position position) {
+        if (!cache) {
+            return BlockTranslator.AIR;
+        }
+        ChunkPosition chunkPosition = new ChunkPosition(position.getX() >> 4, position.getZ() >> 4);
+        if (!chunks.containsKey(chunkPosition))
+            return BlockTranslator.AIR;
+
+        Column column = chunks.get(chunkPosition);
+        Chunk chunk = column.getChunks()[position.getY() >> 4];
+        Position blockPosition = chunkPosition.getChunkBlock(position.getX(), position.getY(), position.getZ());
+        if (chunk != null) {
+            return chunk.get(blockPosition.getX(), blockPosition.getY(), blockPosition.getZ());
+        }
+
+        return BlockTranslator.AIR;
+    }
+
+    public void removeChunk(ChunkPosition position) {
+        if (!cache) {
+            return;
+        }
+        chunks.remove(position);
+    }
+}
