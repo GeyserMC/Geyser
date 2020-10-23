ee8c718c621be0b6c3cde594a7b45004fd70e2e9
RednedEpic <redned235@gmail.com>
2020-10-23 01:25:24 -0500

    Translate emote list packet

--- connector/src/main/java/org/geysermc/connector/network/session/GeyserSession.java
+++ connector/src/main/java/org/geysermc/connector/network/session/GeyserSession.java
@@ -275,6 +275,10 @@ public class GeyserSession implements CommandSender {
     @Setter
     private String lastSignMessage;
 
+    @Setter
+    private List<UUID> selectedEmotes = new ArrayList<>();
+    private final Set<UUID> emotes = new HashSet<>();
+
     private MinecraftProtocol protocol;
 
     public GeyserSession(GeyserConnector connector, BedrockServerSession bedrockServerSession) {
@@ -294,6 +298,8 @@ public class GeyserSession implements CommandSender {
         this.loggedIn = false;
 
         this.inventoryCache.getInventories().put(0, inventory);
+
+        connector.getPlayers().forEach(player -> this.emotes.addAll(player.getEmotes()));
 
         bedrockServerSession.addDisconnectHandler(disconnectReason -> {
             connector.getLogger().info(LanguageUtils.getLocaleStringLog("geyser.network.disconnect", bedrockServerSession.getAddress().getAddress(), disconnectReason));
@@ -768,5 +774,23 @@ public class GeyserSession implements CommandSender {
 
         adventureSettingsPacket.getSettings().addAll(flags);
         sendUpstreamPacket(adventureSettingsPacket);
+    }
+
+    public void refreshEmotes(List<UUID> emotes) {
+        this.selectedEmotes = emotes;
+        this.emotes.addAll(emotes);
+        for (GeyserSession player : connector.getPlayers()) {
+            List<UUID> pieces = new ArrayList<>();
+            for (UUID piece : emotes) {
+                if (!player.getEmotes().contains(piece)) {
+                    this.emotes.add(piece);
+                }
+                pieces.add(piece);
+            }
+            EmoteListPacket emoteList = new EmoteListPacket();
+            emoteList.setRuntimeEntityId(player.getPlayerEntity().getGeyserId());
+            emoteList.getPieceIds().addAll(pieces);
+            player.sendUpstreamPacket(emoteList);
+        }
     }
 }
