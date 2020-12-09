fcd8c6b5b7b74dd9d50b304b4eda59c85b1f2b43
rosiecube <rosiecube@gmail.com>
2020-12-09 16:04:25 +0900

    Fixed Bedrock ItemEntry Y position bug
    Add BASE_OFFSET_Y = 0.125f

--- connector/src/main/java/org/geysermc/connector/entity/ItemEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/ItemEntity.java
@@ -28,18 +28,22 @@ package org.geysermc.connector.entity;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.ItemStack;
 import com.nukkitx.math.vector.Vector3f;
-import com.nukkitx.protocol.bedrock.packet.*;
+import com.nukkitx.protocol.bedrock.packet.AddItemEntityPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.item.ItemTranslator;
 
 public class ItemEntity extends Entity {
-
-    Vector3f positionBeforeSend;
+    static final float BASE_OFFSET_Y = 0.125f;
 
     public ItemEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
         super(entityId, geyserId, entityType, position, motion, rotation);
-        positionBeforeSend = this.position;
+    }
+
+    @Override
+    public void setPosition(Vector3f position)
+    {
+        this.position = position.from(position.getX(), position.getY() + BASE_OFFSET_Y, position.getZ());
     }
 
     @Override
@@ -57,53 +61,5 @@ public class ItemEntity extends Entity {
         }
 
         super.updateBedrockMetadata(entityMetadata, session);
-    }
-
-    @Override
-    public void moveRelative(GeyserSession session, double relX, double relY, double relZ, Vector3f rotation, boolean isOnGround) {
-        setRotation(rotation);
-        setOnGround(isOnGround);
-
-        position = Vector3f.from(position.getX() + relX, position.getY() + relY, position.getZ() + relZ);
-
-        // There was a phenomenon where too constant updates were made,
-        // affecting the velocity value of the item entity,
-        // causing the to break through the floor.
-        // So Update was performed before the update and when the location to be moved is greater than 0.5 meters.
-        // It moved less than 0.5, but if the variable for whether it was on the floor was changed to update its position when it hits the floor, we updated it.
-        if (positionBeforeSend.distanceSquared(position) > 0.25f || onGround != isOnGround)
-        {
-            positionBeforeSend = position;
-
-            MoveEntityAbsolutePacket moveEntityPacket = new MoveEntityAbsolutePacket();
-            moveEntityPacket.setRuntimeEntityId(geyserId);
-            moveEntityPacket.setPosition(Vector3f.from(position.getX(), position.getY() + 0.3f, position.getZ()));
-            moveEntityPacket.setRotation(getBedrockRotation());
-            moveEntityPacket.setOnGround(isOnGround);
-            moveEntityPacket.setTeleported(true);
-
-            session.sendUpstreamPacket(moveEntityPacket);
-        }
-    }
-
-    @Override
-    public void moveAbsolute(GeyserSession session, Vector3f position, Vector3f rotation, boolean isOnGround, boolean teleported) {
-        setPosition(position);
-        setRotation(rotation);
-        setOnGround(isOnGround);
-
-        if (positionBeforeSend.distanceSquared(position) > 0.25f || onGround != isOnGround)
-        {
-            positionBeforeSend = position;
-
-            MoveEntityAbsolutePacket moveEntityPacket = new MoveEntityAbsolutePacket();
-            moveEntityPacket.setRuntimeEntityId(geyserId);
-            moveEntityPacket.setPosition(Vector3f.from(position.getX(), position.getY() + 0.3f, position.getZ()));
-            moveEntityPacket.setRotation(getBedrockRotation());
-            moveEntityPacket.setOnGround(isOnGround);
-            moveEntityPacket.setTeleported(teleported);
-
-            session.sendUpstreamPacket(moveEntityPacket);
-        }
     }
 }
