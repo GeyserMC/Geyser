a5c7c51c5ba7e9d6f047cfcf500b6e514665ad47
DoctorMacc <toy.fighter1@gmail.com>
2020-12-08 21:18:09 -0500

    Update Protocol

--- connector/src/main/java/org/geysermc/connector/network/BedrockProtocol.java
+++ connector/src/main/java/org/geysermc/connector/network/BedrockProtocol.java
@@ -27,6 +27,7 @@ package org.geysermc.connector.network;
 
 import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
 import com.nukkitx.protocol.bedrock.v419.Bedrock_v419;
+import com.nukkitx.protocol.bedrock.v422.Bedrock_v422;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,10 +40,7 @@ public class BedrockProtocol {
      * Default Bedrock codec that should act as a fallback. Should represent the latest available
      * release of the game that Geyser supports.
      */
-    public static final BedrockPacketCodec DEFAULT_BEDROCK_CODEC = Bedrock_v419.V419_CODEC.toBuilder()
-            .protocolVersion(422)
-            .minecraftVersion("1.16.200")
-            .build();
+    public static final BedrockPacketCodec DEFAULT_BEDROCK_CODEC = Bedrock_v422.V422_CODEC;
     /**
      * A list of all supported Bedrock versions that can join Geyser
      */
