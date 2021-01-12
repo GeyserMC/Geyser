0c20404ad3dffceb4ea76b3dbcac20449b9a0cb7
rtm516 <ryantmilner@hotmail.co.uk>
2021-01-12 20:58:47 +0000

    Fix indentation

--- connector/src/main/java/org/geysermc/connector/entity/player/PlayerEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/player/PlayerEntity.java
@@ -103,10 +103,10 @@ public class PlayerEntity extends LivingEntity {
 
         long linkedEntityId = session.getEntityCache().getCachedPlayerEntityLink(entityId);
         if (linkedEntityId != -1) {
-			Entity linkedEntity = session.getEntityCache().getEntityByJavaId(linkedEntityId);
-			if (linkedEntity != null) {
-            	addPlayerPacket.getEntityLinks().add(new EntityLinkData(linkedEntity.getGeyserId(), geyserId, EntityLinkData.Type.RIDER, false, false));
-			}
+            Entity linkedEntity = session.getEntityCache().getEntityByJavaId(linkedEntityId);
+            if (linkedEntity != null) {
+                addPlayerPacket.getEntityLinks().add(new EntityLinkData(linkedEntity.getGeyserId(), geyserId, EntityLinkData.Type.RIDER, false, false));
+            }
         }
 
         valid = true;
