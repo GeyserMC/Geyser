b3068f4dafa652c1cc1db6af86350a5da880de40
davchoo <davchoo@users.noreply.github.com>
2021-07-14 10:35:54 -0400

    Fix item drop animation

--- connector/src/main/java/org/geysermc/connector/entity/ItemEntity.java
+++ connector/src/main/java/org/geysermc/connector/entity/ItemEntity.java
@@ -28,31 +28,21 @@ package org.geysermc.connector.entity;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.EntityMetadata;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.ItemStack;
 import com.nukkitx.math.vector.Vector3f;
+import com.nukkitx.math.vector.Vector3i;
+import com.nukkitx.protocol.bedrock.data.entity.EntityFlag;
 import com.nukkitx.protocol.bedrock.data.inventory.ItemData;
 import com.nukkitx.protocol.bedrock.packet.AddItemEntityPacket;
 import org.geysermc.connector.entity.type.EntityType;
 import org.geysermc.connector.network.session.GeyserSession;
 import org.geysermc.connector.network.translators.item.ItemTranslator;
+import org.geysermc.connector.network.translators.world.block.BlockStateValues;
 
-public class ItemEntity extends Entity {
+public class ItemEntity extends ThrowableEntity {
 
     protected ItemData item;
 
     public ItemEntity(long entityId, long geyserId, EntityType entityType, Vector3f position, Vector3f motion, Vector3f rotation) {
-        super(entityId, geyserId, entityType, position.add(0d, entityType.getOffset(), 0d), motion, rotation);
-    }
-
-    @Override
-    public void setMotion(Vector3f motion) {
-        if (isOnGround())
-            motion = Vector3f.from(motion.getX(), 0, motion.getZ());
-
-        super.setMotion(motion);
-    }
-
-    @Override
-    public void moveAbsolute(GeyserSession session, Vector3f position, Vector3f rotation, boolean isOnGround, boolean teleported) {
-        super.moveAbsolute(session, position.add(0d, this.entityType.getOffset(), 0d), rotation, isOnGround, teleported);
+        super(entityId, geyserId, entityType, position, motion, rotation);
     }
 
     @Override
@@ -69,6 +59,43 @@ public class ItemEntity extends Entity {
         itemPacket.setFromFishing(false);
         itemPacket.setItemInHand(item);
         session.sendUpstreamPacket(itemPacket);
+    }
+
+    @Override
+    public void tick(GeyserSession session) {
+        if (!onGround || (motion.getX() * motion.getX() + motion.getZ() * motion.getZ()) > 0.00001) {
+            float gravity = getGravity(session);
+            motion = motion.down(gravity);
+            moveAbsoluteImmediate(session, position.add(motion), rotation, onGround, false);
+            float drag = getDrag(session);
+            motion = motion.mul(drag, 0.98f, drag);
+        }
+    }
+
+    @Override
+    protected void moveAbsoluteImmediate(GeyserSession session, Vector3f position, Vector3f rotation, boolean isOnGround, boolean teleported) {
+        super.moveAbsoluteImmediate(session, position.up(entityType.getOffset()), Vector3f.ZERO, isOnGround, teleported);
+        this.position = position;
+    }
+
+    @Override
+    protected float getGravity(GeyserSession session) {
+        if (metadata.getFlags().getFlag(EntityFlag.HAS_GRAVITY) && !onGround && !isInWater(session)) {
+            // Gravity can change if the item is in water/lava, but
+            // the server calculates the motion & position for us
+            return 0.04f;
+        }
+        return 0;
+    }
+
+    @Override
+    protected float getDrag(GeyserSession session) {
+        if (onGround) {
+            Vector3i groundBlockPos = position.toInt().down(1);
+            int blockState = session.getConnector().getWorldManager().getBlockAt(session, groundBlockPos);
+            return BlockStateValues.getSlipperiness(blockState) * 0.98f;
+        }
+        return 0.98f;
     }
 
     @Override
