1ad952b5814060957a6b677c3f2773b65cdf6498
Redned <redned235@gmail.com>
2021-07-18 15:20:40 -0500

    Handle locale stuff downloading of main thread and add doc to Object2IntBiMap
    Technically LocaleUtils is not thread safe and people running Geyser for the first time on slow internet connections may see some untranslated messages. However, we were seeing startup times of about 1+ minutes on these slow connections, and it's better that players see untranslated messages for a short period of time rather than having to wait over a minute for the program to start up.
    
    Once the locale is installed, it doesn't need to be redownloaded again (unless there is a game update) and all translated messages will just work once this download is complete without clients needing to relog.

--- connector/src/main/java/org/geysermc/connector/utils/Object2IntBiMap.java
+++ connector/src/main/java/org/geysermc/connector/utils/Object2IntBiMap.java
@@ -37,6 +37,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * A primitive int BiMap implementation built around fastutil to
+ * reduce boxing and the memory footprint. Protocol has a
+ * {@link com.nukkitx.protocol.util.Int2ObjectBiMap} class, but it
+ * does not extend the Map interface making it difficult to utilize
+ * it in for loops and the registry system.
+ *
+ * @param <T> the value
+ */
 public class Object2IntBiMap<T> implements Object2IntMap<T> {
     private final Object2IntMap<T> forwards;
     private final Int2ObjectMap<T> backwards;
