dd5baa3428b536a3e5fd936239fc0bf882f72a67
Baterka <bateriecz@gmail.com>
2021-08-30 19:12:20 +0200

    Reverted to config version to 4 + removed ntp server constant

--- connector/src/main/java/org/geysermc/connector/configuration/GeyserConfiguration.java
+++ connector/src/main/java/org/geysermc/connector/configuration/GeyserConfiguration.java
@@ -38,7 +38,7 @@ import java.util.Map;
 public interface GeyserConfiguration {
 
     // Modify this when you introduce breaking changes into the config
-    int CURRENT_CONFIG_VERSION = 5;
+    int CURRENT_CONFIG_VERSION = 4;
 
     IBedrockConfiguration getBedrock();
 
