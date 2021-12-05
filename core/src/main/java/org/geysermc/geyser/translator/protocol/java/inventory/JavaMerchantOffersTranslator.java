b13ae7495f0e45cd0907bd54466aefc1379f49fe
Kenox <muranelp@gmail.com>
2021-12-05 21:37:48 +0100

    Fix custom merchants not opening

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/inventory/JavaMerchantOffersTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/inventory/JavaMerchantOffersTranslator.java
@@ -39,6 +39,7 @@ import org.geysermc.geyser.entity.type.Entity;
 import org.geysermc.geyser.inventory.Inventory;
 import org.geysermc.geyser.inventory.MerchantContainer;
 import org.geysermc.geyser.session.GeyserSession;
+import org.geysermc.geyser.translator.inventory.InventoryTranslator;
 import org.geysermc.geyser.translator.protocol.PacketTranslator;
 import org.geysermc.geyser.translator.protocol.Translator;
 import org.geysermc.geyser.translator.inventory.item.ItemTranslator;
@@ -46,6 +47,7 @@ import org.geysermc.geyser.registry.type.ItemMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Translator(packet = ClientboundMerchantOffersPacket.class)
 public class JavaMerchantOffersTranslator extends PacketTranslator<ClientboundMerchantOffersPacket> {
@@ -57,79 +59,90 @@ public class JavaMerchantOffersTranslator extends PacketTranslator<ClientboundMe
             return;
         }
 
-        // Retrieve the fake villager involved in the trade, and update its metadata to match with the window information
-        merchantInventory.setVillagerTrades(packet.getTrades());
-        Entity villager = merchantInventory.getVillager();
-        villager.getDirtyMetadata().put(EntityData.TRADE_TIER, packet.getVillagerLevel() - 1);
-        villager.getDirtyMetadata().put(EntityData.MAX_TRADE_TIER, 4);
-        villager.getDirtyMetadata().put(EntityData.TRADE_XP, packet.getExperience());
-        villager.updateBedrockMetadata();
+        // Prepare inventory in such a way that a villager is assigned to the merchant inventory if it does not exist
+        // As it could be, that the inventory is declared as pending due to previous closing inventory -> leads to an incorrect
+        // order of execution, which the delay in combination should handle
+        InventoryTranslator inventoryTranslator = InventoryTranslator
+                .inventoryTranslator(com.github.steveice10.mc.protocol.data.game.inventory.ContainerType.MERCHANT);
 
-        // Construct the packet that opens the trading window
-        UpdateTradePacket updateTradePacket = new UpdateTradePacket();
-        updateTradePacket.setTradeTier(packet.getVillagerLevel() - 1);
-        updateTradePacket.setContainerId((short) packet.getContainerId());
-        updateTradePacket.setContainerType(ContainerType.TRADE);
-        updateTradePacket.setDisplayName(openInventory.getTitle());
-        updateTradePacket.setSize(0);
-        updateTradePacket.setNewTradingUi(true);
-        updateTradePacket.setUsingEconomyTrade(true);
-        updateTradePacket.setPlayerUniqueEntityId(session.getPlayerEntity().getGeyserId());
-        updateTradePacket.setTraderUniqueEntityId(villager.getGeyserId());
+        assert inventoryTranslator != null;
+        inventoryTranslator.prepareInventory(session, merchantInventory);
 
-        NbtMapBuilder builder = NbtMap.builder();
-        boolean addExtraTrade = packet.isRegularVillager() && packet.getVillagerLevel() < 5;
-        List<NbtMap> tags = new ArrayList<>(addExtraTrade ? packet.getTrades().length + 1 : packet.getTrades().length);
-        for (int i = 0; i < packet.getTrades().length; i++) {
-            VillagerTrade trade = packet.getTrades()[i];
-            NbtMapBuilder recipe = NbtMap.builder();
-            recipe.putInt("netId", i + 1);
-            recipe.putInt("maxUses", trade.isTradeDisabled() ? 0 : trade.getMaxUses());
-            recipe.putInt("traderExp", trade.getXp());
-            recipe.putFloat("priceMultiplierA", trade.getPriceMultiplier());
-            recipe.put("sell", getItemTag(session, trade.getOutput(), 0));
-            recipe.putFloat("priceMultiplierB", 0.0f);
-            recipe.putInt("buyCountB", trade.getSecondInput() != null ? trade.getSecondInput().getAmount() : 0);
-            recipe.putInt("buyCountA", trade.getFirstInput().getAmount());
-            recipe.putInt("demand", trade.getDemand());
-            recipe.putInt("tier", packet.getVillagerLevel() > 0 ? packet.getVillagerLevel() - 1 : 0); // -1 crashes client
-            recipe.put("buyA", getItemTag(session, trade.getFirstInput(), trade.getSpecialPrice()));
-            if (trade.getSecondInput() != null) {
-                recipe.put("buyB", getItemTag(session, trade.getSecondInput(), 0));
+        session.scheduleInEventLoop(() -> {
+            // Retrieve the fake villager involved in the trade, and update its metadata to match with the window information
+            merchantInventory.setVillagerTrades(packet.getTrades());
+            Entity villager = merchantInventory.getVillager();
+            villager.getDirtyMetadata().put(EntityData.TRADE_TIER, packet.getVillagerLevel() - 1);
+            villager.getDirtyMetadata().put(EntityData.MAX_TRADE_TIER, 4);
+            villager.getDirtyMetadata().put(EntityData.TRADE_XP, packet.getExperience());
+            villager.updateBedrockMetadata();
+
+            // Construct the packet that opens the trading window
+            UpdateTradePacket updateTradePacket = new UpdateTradePacket();
+            updateTradePacket.setTradeTier(packet.getVillagerLevel() - 1);
+            updateTradePacket.setContainerId((short) packet.getContainerId());
+            updateTradePacket.setContainerType(ContainerType.TRADE);
+            updateTradePacket.setDisplayName(openInventory.getTitle());
+            updateTradePacket.setSize(0);
+            updateTradePacket.setNewTradingUi(true);
+            updateTradePacket.setUsingEconomyTrade(true);
+            updateTradePacket.setPlayerUniqueEntityId(session.getPlayerEntity().getGeyserId());
+            updateTradePacket.setTraderUniqueEntityId(villager.getGeyserId());
+
+            NbtMapBuilder builder = NbtMap.builder();
+            boolean addExtraTrade = packet.isRegularVillager() && packet.getVillagerLevel() < 5;
+            List<NbtMap> tags = new ArrayList<>(addExtraTrade ? packet.getTrades().length + 1 : packet.getTrades().length);
+            for (int i = 0; i < packet.getTrades().length; i++) {
+                VillagerTrade trade = packet.getTrades()[i];
+                NbtMapBuilder recipe = NbtMap.builder();
+                recipe.putInt("netId", i + 1);
+                recipe.putInt("maxUses", trade.isTradeDisabled() ? 0 : trade.getMaxUses());
+                recipe.putInt("traderExp", trade.getXp());
+                recipe.putFloat("priceMultiplierA", trade.getPriceMultiplier());
+                recipe.put("sell", getItemTag(session, trade.getOutput(), 0));
+                recipe.putFloat("priceMultiplierB", 0.0f);
+                recipe.putInt("buyCountB", trade.getSecondInput() != null ? trade.getSecondInput().getAmount() : 0);
+                recipe.putInt("buyCountA", trade.getFirstInput().getAmount());
+                recipe.putInt("demand", trade.getDemand());
+                recipe.putInt("tier", packet.getVillagerLevel() > 0 ? packet.getVillagerLevel() - 1 : 0); // -1 crashes client
+                recipe.put("buyA", getItemTag(session, trade.getFirstInput(), trade.getSpecialPrice()));
+                if (trade.getSecondInput() != null) {
+                    recipe.put("buyB", getItemTag(session, trade.getSecondInput(), 0));
+                }
+                recipe.putInt("uses", trade.getNumUses());
+                recipe.putByte("rewardExp", (byte) 1);
+                tags.add(recipe.build());
             }
-            recipe.putInt("uses", trade.getNumUses());
-            recipe.putByte("rewardExp", (byte) 1);
-            tags.add(recipe.build());
-        }
 
-        //Hidden trade to fix visual experience bug
-        if (addExtraTrade) {
-            tags.add(NbtMap.builder()
-                    .putInt("maxUses", 0)
-                    .putInt("traderExp", 0)
-                    .putFloat("priceMultiplierA", 0.0f)
-                    .putFloat("priceMultiplierB", 0.0f)
-                    .putInt("buyCountB", 0)
-                    .putInt("buyCountA", 0)
-                    .putInt("demand", 0)
-                    .putInt("tier", 5)
-                    .putInt("uses", 0)
-                    .putByte("rewardExp", (byte) 0)
-                    .build());
-        }
+            //Hidden trade to fix visual experience bug
+            if (addExtraTrade) {
+                tags.add(NbtMap.builder()
+                        .putInt("maxUses", 0)
+                        .putInt("traderExp", 0)
+                        .putFloat("priceMultiplierA", 0.0f)
+                        .putFloat("priceMultiplierB", 0.0f)
+                        .putInt("buyCountB", 0)
+                        .putInt("buyCountA", 0)
+                        .putInt("demand", 0)
+                        .putInt("tier", 5)
+                        .putInt("uses", 0)
+                        .putByte("rewardExp", (byte) 0)
+                        .build());
+            }
 
-        builder.putList("Recipes", NbtType.COMPOUND, tags);
+            builder.putList("Recipes", NbtType.COMPOUND, tags);
 
-        List<NbtMap> expTags = new ArrayList<>(5);
-        expTags.add(NbtMap.builder().putInt("0", 0).build());
-        expTags.add(NbtMap.builder().putInt("1", 10).build());
-        expTags.add(NbtMap.builder().putInt("2", 70).build());
-        expTags.add(NbtMap.builder().putInt("3", 150).build());
-        expTags.add(NbtMap.builder().putInt("4", 250).build());
-        builder.putList("TierExpRequirements", NbtType.COMPOUND, expTags);
+            List<NbtMap> expTags = new ArrayList<>(5);
+            expTags.add(NbtMap.builder().putInt("0", 0).build());
+            expTags.add(NbtMap.builder().putInt("1", 10).build());
+            expTags.add(NbtMap.builder().putInt("2", 70).build());
+            expTags.add(NbtMap.builder().putInt("3", 150).build());
+            expTags.add(NbtMap.builder().putInt("4", 250).build());
+            builder.putList("TierExpRequirements", NbtType.COMPOUND, expTags);
 
-        updateTradePacket.setOffers(builder.build());
-        session.sendUpstreamPacket(updateTradePacket);
+            updateTradePacket.setOffers(builder.build());
+            session.sendUpstreamPacket(updateTradePacket);
+        }, 200, TimeUnit.MILLISECONDS);
     }
 
     private NbtMap getItemTag(GeyserSession session, ItemStack stack, int specialPrice) {
