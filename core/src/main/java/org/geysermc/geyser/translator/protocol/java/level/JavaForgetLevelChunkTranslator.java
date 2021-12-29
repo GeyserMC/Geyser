61636f44aa1d144c05bd95e3b51d7ca7e416db19
RednedEpic <redned235@gmail.com>
2021-12-28 23:25:33 -0600

    Simulate Java chunk unloading by sending empty chunks Closes #2725

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/level/JavaForgetLevelChunkTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/level/JavaForgetLevelChunkTranslator.java
@@ -30,6 +30,7 @@ import com.nukkitx.math.vector.Vector3i;
 import org.geysermc.geyser.session.GeyserSession;
 import org.geysermc.geyser.translator.protocol.PacketTranslator;
 import org.geysermc.geyser.translator.protocol.Translator;
+import org.geysermc.geyser.util.ChunkUtils;
 
 import java.util.Iterator;
 
@@ -60,5 +61,7 @@ public class JavaForgetLevelChunkTranslator extends PacketTranslator<Clientbound
                 }
             }
         }
+
+        ChunkUtils.sendEmptyChunk(session, packet.getX(), packet.getZ(), false);
     }
 }
