65b68087b84aaccc78fc3664ace7ed04a993adc6
Camotoy <20743703+Camotoy@users.noreply.github.com>
2022-02-28 10:07:45 -0500

    Bump Geyser to 2.0.2 and Java to 1.18.2

--- core/src/main/java/org/geysermc/geyser/network/MinecraftProtocol.java
+++ core/src/main/java/org/geysermc/geyser/network/MinecraftProtocol.java
@@ -32,10 +32,7 @@ import com.nukkitx.protocol.bedrock.v471.Bedrock_v471;
 import com.nukkitx.protocol.bedrock.v475.Bedrock_v475;
 import com.nukkitx.protocol.bedrock.v486.Bedrock_v486;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
 /**
  * Contains information about the supported protocols in Geyser.
@@ -92,7 +89,7 @@ public final class MinecraftProtocol {
      * @return the supported Minecraft: Java Edition version names
      */
     public static List<String> getJavaVersions() {
-        return Arrays.asList("1.18", "1.18.1");
+        return Collections.singletonList(DEFAULT_JAVA_CODEC.getMinecraftVersion());
     }
 
     /**
