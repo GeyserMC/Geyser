0163e3369314b50c67e0aa514a57fad792c5d94f
rtm516 <rtm516@users.noreply.github.com>
2022-04-24 18:02:02 +0100

    Re-arrange groups and artefacts when publishing (#2899)
    * Re-arrange groups and artefacts when publishing
    
    * Publish core jar

--- api/geyser/build.gradle.kts
+++ api/geyser/build.gradle.kts
@@ -1,3 +1,10 @@
 dependencies {
     api(projects.api)
 }
+
+publishing {
+    publications.named<MavenPublication>("mavenJava") {
+        groupId = rootProject.group as String + ".geyser"
+        artifactId = "api"
+    }
+}
