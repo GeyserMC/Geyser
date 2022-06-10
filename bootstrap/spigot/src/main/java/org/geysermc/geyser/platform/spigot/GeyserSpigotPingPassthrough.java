691d674f018ff25231a6bd018401e6bfc04e4e23
Camotoy <20743703+Camotoy@users.noreply.github.com>
2022-06-09 21:40:54 -0400

    Use PaperServerListPingEvent when available
    We need to support this or else events that exclusively use PaperServerListPingEvent will not see our call.
    
    Fixes https://github.com/GeyserMC/Geyser/issues/3003

--- bootstrap/spigot/src/main/java/org/geysermc/geyser/platform/spigot/GeyserSpigotPingPassthrough.java
+++ bootstrap/spigot/src/main/java/org/geysermc/geyser/platform/spigot/GeyserSpigotPingPassthrough.java
@@ -30,8 +30,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.util.CachedServerIcon;
-import org.geysermc.geyser.ping.GeyserPingInfo;
 import org.geysermc.geyser.network.MinecraftProtocol;
+import org.geysermc.geyser.ping.GeyserPingInfo;
 import org.geysermc.geyser.ping.IGeyserPingPassthrough;
 
 import javax.annotation.Nonnull;
