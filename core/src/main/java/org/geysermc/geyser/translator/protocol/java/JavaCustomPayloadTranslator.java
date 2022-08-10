ab2b79485b7c2eeccd1c4994260bf631af712c45
Konicai <71294714+Konicai@users.noreply.github.com>
2022-08-10 16:09:55 -0400

    Fix sending forms with floodgate for 1.19.20 (#3217)
    * Fix sending forms with floodgate
    
    * Comment about 1.19.20
    
    * Swapped if-else
    
    Co-authored-by: Tim203 <mctim203@gmail.com>

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/JavaCustomPayloadTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/JavaCustomPayloadTranslator.java
@@ -72,12 +72,19 @@ public class JavaCustomPayloadTranslator extends PacketTranslator<ClientboundCus
             String dataString = new String(data, 3, data.length - 3, Charsets.UTF_8);
 
             Form form = Forms.fromJson(dataString, type, (ignored, response) -> {
-                byte[] raw = response.getBytes(StandardCharsets.UTF_8);
-                byte[] finalData = new byte[raw.length + 2];
+                byte[] finalData;
+                if (response == null) {
+                    // Response data can be null as of 1.19.20 (same behaviour as empty response data)
+                    // Only need to send the form id
+                    finalData = new byte[]{data[1], data[2]};
+                } else {
+                    byte[] raw = response.getBytes(StandardCharsets.UTF_8);
+                    finalData = new byte[raw.length + 2];
 
-                finalData[0] = data[1];
-                finalData[1] = data[2];
-                System.arraycopy(raw, 0, finalData, 2, raw.length);
+                    finalData[0] = data[1];
+                    finalData[1] = data[2];
+                    System.arraycopy(raw, 0, finalData, 2, raw.length);
+                }
 
                 session.sendDownstreamPacket(new ServerboundCustomPayloadPacket(channel, finalData));
             });
