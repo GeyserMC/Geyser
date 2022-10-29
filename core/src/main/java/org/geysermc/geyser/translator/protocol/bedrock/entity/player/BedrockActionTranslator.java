a38a7113aef907a6ea2a1a1370e4e81462825170
Konicai <71294714+Konicai@users.noreply.github.com>
2022-10-29 18:22:10 -0400

    Fix bounding box for 1.19.40 after death + respawn
    Co-authored-by: onebeastchris <105284508+onebeastchris@users.noreply.github.com>

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockActionTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockActionTranslator.java
@@ -75,6 +75,10 @@ public class BedrockActionTranslator extends PacketTranslator<PlayerActionPacket
                 attributesPacket.setRuntimeEntityId(entity.getGeyserId());
                 attributesPacket.getAttributes().addAll(entity.getAttributes().values());
                 session.sendUpstreamPacket(attributesPacket);
+
+                // Bounding box must be sent after a player dies and respawns since 1.19.40
+                entity.refreshBoundingBox();
+                entity.updateBedrockMetadata();
                 break;
             case START_SWIMMING:
                 if (!entity.getFlag(EntityFlag.SWIMMING)) {
