65319c58597e73b8b8f0d8162f60fa48fc6aa8a9
Camotoy <20743703+Camotoy@users.noreply.github.com>
2023-02-24 12:40:29 -0500

    Prepare for an actual 568 release

--- core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
+++ core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
@@ -34,6 +34,7 @@ import com.nukkitx.protocol.bedrock.v554.Bedrock_v554;
 import com.nukkitx.protocol.bedrock.v557.Bedrock_v557;
 import com.nukkitx.protocol.bedrock.v560.Bedrock_v560;
 import com.nukkitx.protocol.bedrock.v567.Bedrock_v567;
+import com.nukkitx.protocol.bedrock.v567.Bedrock_v567patch;
 import org.geysermc.geyser.session.GeyserSession;
 
 import java.util.ArrayList;
@@ -75,8 +76,7 @@ public final class GameProtocol {
                 .minecraftVersion("1.19.50/1.19.51")
                 .build());
         SUPPORTED_BEDROCK_CODECS.add(DEFAULT_BEDROCK_CODEC);
-        // So the version checker will work
-        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v567.V567_CODEC.toBuilder()
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v567patch.BEDROCK_V567PATCH.toBuilder()
                 .protocolVersion(568)
                 .minecraftVersion("1.19.62")
                 .build());
