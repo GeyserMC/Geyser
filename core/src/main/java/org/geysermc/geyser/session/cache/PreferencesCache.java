b4975e2c27d3738b8a0a57da045b4a24b6d63447
Jonathan Hiles <jonathan@hil.es>
2023-04-19 14:50:40 +1000

    make 'above-bedrock-nether-building' player preference default to enabled

--- core/src/main/java/org/geysermc/geyser/session/cache/PreferencesCache.java
+++ core/src/main/java/org/geysermc/geyser/session/cache/PreferencesCache.java
@@ -69,6 +69,7 @@ public class PreferencesCache {
         this.session = session;
 
         prefersCustomSkulls = session.getGeyser().getConfig().isAllowCustomSkulls();
+        prefersAboveBedrockNetherBuilding = session.getGeyser().getConfig().isAboveBedrockNetherBuilding();
     }
 
     /**
