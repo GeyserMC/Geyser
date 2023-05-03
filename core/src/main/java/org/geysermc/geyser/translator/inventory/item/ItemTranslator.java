c144819486df3f1887c97ad51327d047e49e1bd7
YHDiamond <yehudahrrs@gmail.com>
2023-05-03 18:48:20 -0400

    Add null check for NBT

--- core/src/main/java/org/geysermc/geyser/translator/inventory/item/ItemTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/inventory/item/ItemTranslator.java
@@ -116,7 +116,7 @@ public final class ItemTranslator {
 
         nbt = translateDisplayProperties(session, nbt, bedrockItem);
 
-        if (nbt.contains("AttributeModifiers")) {
+        if (nbt != null && nbt.contains("AttributeModifiers")) {
             addAttributes(nbt, javaItem, session.locale());
         }
 
