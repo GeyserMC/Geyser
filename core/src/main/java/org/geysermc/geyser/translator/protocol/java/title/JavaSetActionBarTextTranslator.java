65d7c8a7506c896854d779574cef1d3d35f31ee2
onebeastchris <github@onechris.mozmail.com>
2023-07-05 17:14:43 +0200

    send actionbar and systemchat overlay messages as jukebox text to avoid clashes with armor/air UI

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/title/JavaSetActionBarTextTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/title/JavaSetActionBarTextTranslator.java
@@ -26,7 +26,7 @@
 package org.geysermc.geyser.translator.protocol.java.title;
 
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.title.ClientboundSetActionBarTextPacket;
-import org.cloudburstmc.protocol.bedrock.packet.SetTitlePacket;
+import org.cloudburstmc.protocol.bedrock.packet.TextPacket;
 import org.geysermc.geyser.session.GeyserSession;
 import org.geysermc.geyser.translator.protocol.PacketTranslator;
 import org.geysermc.geyser.translator.protocol.Translator;
@@ -44,11 +44,13 @@ public class JavaSetActionBarTextTranslator extends PacketTranslator<Clientbound
             text = MessageTranslator.convertMessage(packet.getText(), session.locale());
         }
 
-        SetTitlePacket titlePacket = new SetTitlePacket();
-        titlePacket.setType(SetTitlePacket.Type.ACTIONBAR);
-        titlePacket.setText(text);
-        titlePacket.setXuid("");
-        titlePacket.setPlatformOnlineId("");
-        session.sendUpstreamPacket(titlePacket);
+        TextPacket textPacket = new TextPacket();
+        textPacket.setType(TextPacket.Type.JUKEBOX_POPUP);
+        textPacket.setNeedsTranslation(false);
+        textPacket.setSourceName("");
+        textPacket.setMessage(text);
+        textPacket.setXuid("");
+        textPacket.setPlatformChatId("");
+        session.sendUpstreamPacket(textPacket);
     }
 }
