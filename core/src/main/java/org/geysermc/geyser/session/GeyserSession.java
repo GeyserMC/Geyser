3a92d5bfe302d397934308b89eb06e9da12218a0
Konicai <71294714+Konicai@users.noreply.github.com>
2023-07-22 18:16:18 -0400

    Start with small queue capacity

--- core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
+++ core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
@@ -569,7 +569,7 @@ public class GeyserSession implements GeyserConnection, GeyserCommandSource {
      * A cache of IDs from ClientboundKeepAlivePackets that have been sent to the Bedrock client, but haven't been
      * returned to the server. Only used if {@link GeyserConfiguration#isForwardPlayerPing()} is enabled.
      */
-    private final Queue<Long> keepAliveCache = new ArrayDeque<>();
+    private final Queue<Long> keepAliveCache = new ArrayDeque<>(3);
 
     private MinecraftProtocol protocol;
 
