91fa5d5824299238d1ccead79c3070a20d7e3230
onebeastchris <github@onechris.mozmail.com>
2023-08-30 23:45:29 +0200

    oops, not yet needed

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockSetDefaultGameTypeTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockSetDefaultGameTypeTranslator.java
@@ -46,8 +46,7 @@ public class BedrockSetDefaultGameTypeTranslator extends PacketTranslator<SetDef
         // Stop the client from updating their own Gamemode without telling the server
         // Can occur when client Gamemode is set to "default", and default game mode is changed.
         SetPlayerGameTypePacket playerGameTypePacket = new SetPlayerGameTypePacket();
-        int gamemode = session.getGameMode().ordinal() == 3 ? 6 : session.getGameMode().ordinal();
-        playerGameTypePacket.setGamemode(gamemode);
+        playerGameTypePacket.setGamemode(session.getGameMode().ordinal());
         session.sendUpstreamPacket(playerGameTypePacket);
     }
 }
