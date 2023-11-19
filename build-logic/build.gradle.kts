8594ee17442132e058412766c37af01990cfc198
onebeastchris <github@onechris.mozmail.com>
2023-11-19 01:03:25 +0100

    Bump lombok, architectury-loom

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("net.kyori", "indra-common", "3.1.1")
     implementation("com.github.johnrengelman", "shadow", "7.1.3-SNAPSHOT")
     implementation("architectury-plugin", "architectury-plugin.gradle.plugin", "3.4-SNAPSHOT")
-    implementation("dev.architectury.loom", "dev.architectury.loom.gradle.plugin", "1.1-SNAPSHOT")
+    implementation("dev.architectury.loom", "dev.architectury.loom.gradle.plugin", "1.4-SNAPSHOT")
     implementation("com.modrinth.minotaur:Minotaur:2.7.5")
 
     // Within the gradle plugin classpath, there is a version conflict between loom and some other
