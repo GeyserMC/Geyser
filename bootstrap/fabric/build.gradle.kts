75fe94e0a588067d5c8d7a697587f9c766c57ddc
Hasan <52300558+hasankayra04@users.noreply.github.com>
2023-11-21 09:10:11 +0300

    Update versions in README and modrinth task (#4303)
    * Change fabric version
    
    * Update java version
    
    * Update Bedrock version also

--- bootstrap/fabric/build.gradle.kts
+++ bootstrap/fabric/build.gradle.kts
@@ -119,7 +119,7 @@ modrinth {
     syncBodyFrom.set(rootProject.file("README.md").readText())
 
     uploadFile.set(tasks.getByPath("remapModrinthJar"))
-    gameVersions.addAll("1.20.2")
+    gameVersions.addAll("1.20.3")
 
     loaders.add("fabric")
     failSilently.set(true)
