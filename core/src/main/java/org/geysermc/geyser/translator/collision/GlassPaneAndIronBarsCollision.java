cb32e943739f291d2aa13c570c356196555b1465
Miku_Snow <gal499605076@gmail.com>
2023-12-07 01:10:54 +0800

    More lenient judgment; reduce redundant code to avoid repeated calls.

--- core/src/main/java/org/geysermc/geyser/translator/collision/GlassPaneAndIronBarsCollision.java
+++ core/src/main/java/org/geysermc/geyser/translator/collision/GlassPaneAndIronBarsCollision.java
@@ -47,22 +47,22 @@ public class GlassPaneAndIronBarsCollision extends BlockCollision {
     public GlassPaneAndIronBarsCollision(String params, BoundingBox[] defaultBoxes) {
         super(defaultBoxes);
         //east=true,north=true,south=true,west=true
-        if (params.contains("north=true") && params.contains("east=false") && params.contains("south=false") && params.contains("west=false")) {
-            facing = 1;
-        } else if (params.contains("north=true") && params.contains("east=true") && params.contains("south=false") && params.contains("west=false")) {
+        if (params.contains("north=true") && params.contains("east=true")) {
             facing = 5;
-        } else if (params.contains("east=true") && params.contains("north=false") && params.contains("south=false") && params.contains("west=false")) {
-            facing = 2;
-        } else if (params.contains("east=true") && params.contains("north=false") && params.contains("south=true") && params.contains("west=false")) {
+        } else if (params.contains("east=true") && params.contains("south=true")) {
             facing = 6;
-        } else if (params.contains("south=true") && params.contains("north=false") && params.contains("east=false") && params.contains("west=false")) {
-            facing = 3;
-        } else if (params.contains("south=true") && params.contains("north=false") && params.contains("east=false") && params.contains("west=true")) {
+        } else if (params.contains("south=true") && params.contains("west=true")) {
             facing = 7;
-        } else if (params.contains("west=true") && params.contains("north=false") && params.contains("east=false") && params.contains("south=false")) {
-            facing = 4;
-        } else if (params.contains("west=true") && params.contains("north=true") && params.contains("east=false") && params.contains("south=false")) {
+        } else if (params.contains("west=true") && params.contains("north=true")) {
             facing = 8;
+        } else if (params.contains("north=true")) {
+            facing = 1;
+        } else if (params.contains("east=true")) {
+            facing = 2;
+        } else if (params.contains("south=true")) {
+            facing = 3;
+        } else if (params.contains("west=true")) {
+            facing = 4;
         }
     }
 
@@ -74,28 +74,34 @@ public class GlassPaneAndIronBarsCollision extends BlockCollision {
         playerCollision.setSizeZ(playerCollision.getSizeZ() - 0.0001);
 
         if (this.checkIntersection(x, y, z, playerCollision)) {
+            double newMiddleX = x;
+            double newMiddleZ = z;
+
             switch (facing) {
-                case 1 -> playerCollision.setMiddleZ(z + 0.8625); // North
-                case 2 -> playerCollision.setMiddleX(x + 0.1375); // East
-                case 3 -> playerCollision.setMiddleZ(z + 0.1375); // South
-                case 4 -> playerCollision.setMiddleX(x + 0.8625); // West
-                case 5 -> { //North, East
-                    playerCollision.setMiddleZ(z + 0.8625);
-                    playerCollision.setMiddleX(x + 0.1375);
+                case 1 -> newMiddleZ += 0.8625; // North
+                case 2 -> newMiddleX += 0.1375; // East
+                case 3 -> newMiddleZ += 0.1375; // South
+                case 4 -> newMiddleX += 0.8625; // West
+                case 5 -> { // North, East
+                    newMiddleZ += 0.8625;
+                    newMiddleX += 0.1375;
                 }
-                case 6 -> { //East, South
-                    playerCollision.setMiddleX(x + 0.1375);
-                    playerCollision.setMiddleZ(z + 0.1375);
+                case 6 -> { // East, South
+                    newMiddleX += 0.1375;
+                    newMiddleZ += 0.1375;
                 }
-                case 7 -> { //South, West
-                    playerCollision.setMiddleZ(z + 0.1375);
-                    playerCollision.setMiddleX(x + 0.8625);
+                case 7 -> { // South, West
+                    newMiddleZ += 0.1375;
+                    newMiddleX += 0.8625;
                 }
-                case 8 -> { //West, North
-                    playerCollision.setMiddleX(x + 0.8625);
-                    playerCollision.setMiddleZ(z + 0.8625);
+                case 8 -> { // West, North
+                    newMiddleX += 0.8625;
+                    newMiddleZ += 0.8625;
                 }
             }
+
+            playerCollision.setMiddleX(newMiddleX);
+            playerCollision.setMiddleZ(newMiddleZ);
         }
 
         playerCollision.setSizeX(playerCollision.getSizeX() + 0.0001);
