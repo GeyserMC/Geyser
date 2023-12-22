303327a388dbda06aaacc49274b468aaee0203ac
onebeastchris <github@onechris.mozmail.com>
2023-12-22 14:40:15 +0100

    oops

--- core/src/main/java/org/geysermc/geyser/util/WebUtils.java
+++ core/src/main/java/org/geysermc/geyser/util/WebUtils.java
@@ -31,7 +31,12 @@ import org.geysermc.geyser.GeyserImpl;
 
 import javax.naming.directory.Attribute;
 import javax.naming.directory.InitialDirContext;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
