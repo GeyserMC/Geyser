da5c72b03341aff3b88ebabb870527742d05bbcd
onebeastchris <github@onechris.mozmail.com>
2024-01-22 19:10:27 +0100

    wrap debug message in debug check

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/JavaUpdateRecipesTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/JavaUpdateRecipesTranslator.java
@@ -170,8 +170,10 @@ public class JavaUpdateRecipesTranslator extends PacketTranslator<ClientboundUpd
                 case STONECUTTING -> {
                     StoneCuttingRecipeData stoneCuttingData = (StoneCuttingRecipeData) recipe.getData();
                     if (stoneCuttingData.getIngredient().getOptions().length == 0) {
-                        GeyserImpl.getInstance().getLogger().debug("Received broken stone cutter recipe: " + stoneCuttingData + " " +
-                                recipe.getIdentifier() + " " + Registries.JAVA_ITEMS.get().get(stoneCuttingData.getResult().getId()).javaIdentifier());
+                        if (GeyserImpl.getInstance().getConfig().isDebugMode()) {
+                            GeyserImpl.getInstance().getLogger().debug("Received broken stone cutter recipe: " + stoneCuttingData + " " +
+                                    recipe.getIdentifier() + " " + Registries.JAVA_ITEMS.get().get(stoneCuttingData.getResult().getId()).javaIdentifier());
+                        }
                         continue;
                     }
                     ItemStack ingredient = stoneCuttingData.getIngredient().getOptions()[0];
