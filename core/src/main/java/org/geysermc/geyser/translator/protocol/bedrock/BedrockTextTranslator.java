0980547da228f510595254471480a56b94dd064f
onebeastchris <github@onechris.mozmail.com>
2024-05-29 21:42:07 +0200

    Ensure we send commands/chat the same way a Java client would

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/BedrockTextTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/BedrockTextTranslator.java
@@ -31,12 +31,21 @@ import org.geysermc.geyser.translator.protocol.PacketTranslator;
 import org.geysermc.geyser.translator.protocol.Translator;
 import org.geysermc.geyser.translator.text.MessageTranslator;
 
+import static org.geysermc.geyser.translator.text.MessageTranslator.normalizeSpace;
+
 @Translator(packet = TextPacket.class)
 public class BedrockTextTranslator extends PacketTranslator<TextPacket> {
 
     @Override
     public void translate(GeyserSession session, TextPacket packet) {
-        String message = MessageTranslator.convertToPlainText(packet.getMessage());
+        // Java trims all messages, and then checks for the leading slash
+        String message = MessageTranslator.convertToPlainText(normalizeSpace(packet.getMessage()));
+
+        if (message.startsWith("/")) {
+            // Yes, Java actually allows whitespaces before commands and will still see those as valid
+            BedrockCommandRequestTranslator.handleCommand(session, message.substring(1));
+            return;
+        }
 
         if (message.isBlank()) {
             // Java Edition (as of 1.17.1) just doesn't pass on these messages, so... we won't either!
