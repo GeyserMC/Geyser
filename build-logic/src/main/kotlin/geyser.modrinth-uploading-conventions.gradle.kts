f70aba7dbac5ca15c9b0a25cef67e87142ed512a
onebeastchris <github@onechris.mozmail.com>
2024-08-24 18:57:32 +0200

    Feature: Version names on modrinth published builds

--- build-logic/src/main/kotlin/geyser.modrinth-uploading-conventions.gradle.kts
+++ build-logic/src/main/kotlin/geyser.modrinth-uploading-conventions.gradle.kts
@@ -8,7 +8,8 @@ tasks.modrinth.get().dependsOn(tasks.modrinthSyncBody)
 modrinth {
     token.set(System.getenv("MODRINTH_TOKEN") ?: "") // Even though this is the default value, apparently this prevents GitHub Actions caching the token?
     projectId.set("geyser")
-    versionNumber.set(project.version as String + "-" + System.getenv("BUILD_NUMBER"))
+    versionName.set(versionName())
+    versionNumber.set(version())
     versionType.set("beta")
     changelog.set(System.getenv("CHANGELOG") ?: "")
     gameVersions.addAll("1.21", libs.minecraft.get().version as String)
@@ -16,3 +17,9 @@ modrinth {
 
     syncBodyFrom.set(rootProject.file("README.md").readText())
 }
+
+private fun version(): String =
+    project.version.toString().removeSuffix("SNAPSHOT") + "b" + System.getenv("BUILD_NUMBER")
+
+private fun versionName(): String =
+    "Geyser-" + project.name.replaceFirstChar { it.uppercase() } + "-" + version()
