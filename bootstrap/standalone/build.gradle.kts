7c474596096610f38f47985a3d4dd135f49a43d0
chris <github@onechris.mozmail.com>
2025-05-22 17:43:02 +0200

    Resolve memory usage issues related to Netty 4.2 update (#5536)
    * More proper workaround for direct connection using Netty 4.2
    
    * Add check for outdated Velocity
    
    * BungeeCord support for Netty 4.2
    
    * Use pooled allocator
    
    * Small cleanup: only one allocator system property check, pin comment to static commit
    
    * Set allocator type on Standalone using netty system property
    
    * Don't mess with allocator, both BungeeCord and Velocity set it globally
    
    * Temporarily remove io_uring support
    
    In order to support io_uring transport after the 4.2 update, we'd need to separately handle 4.1 and 4.2 event loop group creation
    
    * Add compileOnly netty io_uring dependency for Geyser-BungeeCord
    
    * Exclude old incubator io_uring dependency on geyser-standalone
    
    ---------
    
    Co-authored-by: Camotoy <20743703+Camotoy@users.noreply.github.com>

--- bootstrap/standalone/build.gradle.kts
+++ bootstrap/standalone/build.gradle.kts
@@ -32,6 +32,11 @@ tasks.named<Jar>("jar") {
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     archiveBaseName.set("Geyser-Standalone")
 
+    // temporary measure - incubator's io_uring is not compatible with 4.2.1
+    dependencies {
+        exclude(dependency("io.netty.incubator:.*"))
+    }
+
     transform(Log4j2PluginsCacheFileTransformer())
 }
 
