b58c9db9b83b5dc4b5a5052a0ac4cb8083aaa601
onebeastchris <github@onechris.mozmail.com>
2025-07-25 02:54:59 +0200

    Initial work on 1.21.100

--- core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
+++ core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
@@ -31,6 +31,7 @@ import org.cloudburstmc.protocol.bedrock.codec.v786.Bedrock_v786;
 import org.cloudburstmc.protocol.bedrock.codec.v800.Bedrock_v800;
 import org.cloudburstmc.protocol.bedrock.codec.v818.Bedrock_v818;
 import org.cloudburstmc.protocol.bedrock.codec.v819.Bedrock_v819;
+import org.cloudburstmc.protocol.bedrock.codec.v827.Bedrock_v827;
 import org.cloudburstmc.protocol.bedrock.netty.codec.packet.BedrockPacketCodec;
 import org.geysermc.geyser.session.GeyserSession;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftCodec;
@@ -49,8 +50,8 @@ public final class GameProtocol {
      * Default Bedrock codec that should act as a fallback. Should represent the latest available
      * release of the game that Geyser supports.
      */
-    public static final BedrockCodec DEFAULT_BEDROCK_CODEC = CodecProcessor.processCodec(Bedrock_v819.CODEC.toBuilder()
-        .minecraftVersion("1.21.93")
+    public static final BedrockCodec DEFAULT_BEDROCK_CODEC = CodecProcessor.processCodec(Bedrock_v827.CODEC.toBuilder()
+        .minecraftVersion("1.21.100")
         .build());
 
     /**
@@ -74,6 +75,9 @@ public final class GameProtocol {
         SUPPORTED_BEDROCK_CODECS.add((CodecProcessor.processCodec(Bedrock_v818.CODEC.toBuilder()
             .minecraftVersion("1.21.90 - 1.21.92")
             .build())));
+        SUPPORTED_BEDROCK_CODECS.add(CodecProcessor.processCodec(Bedrock_v819.CODEC.toBuilder()
+            .minecraftVersion("1.21.93")
+            .build()));
         SUPPORTED_BEDROCK_CODECS.add(DEFAULT_BEDROCK_CODEC);
     }
 
