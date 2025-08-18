85d26af7972c8d3b6933a4a8b55759cdf20b1106
oryxel1 <gamingnoob681@gmail.com>
2025-08-18 17:34:07 +0700

    Use Math.toRadians.

--- core/src/main/java/org/geysermc/geyser/translator/protocol/java/entity/player/JavaPlayerPositionTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/java/entity/player/JavaPlayerPositionTranslator.java
@@ -130,7 +130,7 @@ public class JavaPlayerPositionTranslator extends PacketTranslator<ClientboundPl
         );
 
         if (packet.getRelatives().contains(PositionElement.ROTATE_DELTA)) {
-            deltaMovement = MathUtils.xYRot(deltaMovement, (lastPlayerPitch - newPitch) * 0.017453292519943295F, (lastPlayerYaw - newYaw) * 0.017453292519943295F);
+            deltaMovement = MathUtils.xYRot(deltaMovement, Math.toRadians(lastPlayerPitch - newPitch), Math.toRadians(lastPlayerYaw - newYaw));
         }
 
         entity.moveAbsolute(teleportDestination, newYaw, newPitch, false, true);
