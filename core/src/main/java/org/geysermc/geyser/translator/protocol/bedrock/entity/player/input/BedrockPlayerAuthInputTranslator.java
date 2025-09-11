35be9072c7350b56afdb3c9365ed22de0adb3796
chris <github@onechris.mozmail.com>
2025-09-11 14:51:09 +0200

    Improve block breaking code (#5809)
    * Feature: Guessing how long block breaking will take
    
    * more changes
    
    * Remove BedrockBlockActions.java
    
    * More progress: Remove code that causes us to send erroneous early block breaking actions
    
    * Further work on insta-break handling
    
    * Add GAME_MASTER_BLOCK check
    
    * Cleanup, this actually works better than expected
    
    * Code cleanup
    
    * Inverted valid check will do invalid things!
    
    * Fix block breaking attribute reading
    
    * Implement adventure mode can_break predicates
    
    * Address reviews, minor changes to wonderful code
    
    * Remove JavaBlockBreakHandler.java in favor of extension
    
    * yeet debug
    
    * Avoid dividing by zero, fix item frame interactions for good
    
    * Also avoid dividing by zero here
    
    ---------
    
    Co-authored-by: Eclipse <eclipse@eclipseisoffline.xyz>

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/input/BedrockPlayerAuthInputTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/input/BedrockPlayerAuthInputTranslator.java
@@ -29,45 +29,34 @@ import org.cloudburstmc.math.GenericMath;
 import org.cloudburstmc.math.vector.Vector2f;
 import org.cloudburstmc.math.vector.Vector3f;
 import org.cloudburstmc.protocol.bedrock.data.InputMode;
-import org.cloudburstmc.protocol.bedrock.data.LevelEvent;
 import org.cloudburstmc.protocol.bedrock.data.PlayerAuthInputData;
 import org.cloudburstmc.protocol.bedrock.data.entity.EntityFlag;
 import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.ItemUseTransaction;
 import org.cloudburstmc.protocol.bedrock.packet.AnimatePacket;
-import org.cloudburstmc.protocol.bedrock.packet.LevelEventPacket;
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket;
 import org.cloudburstmc.protocol.bedrock.packet.UpdateAttributesPacket;
-import org.geysermc.geyser.entity.EntityDefinitions;
 import org.geysermc.geyser.entity.type.BoatEntity;
 import org.geysermc.geyser.entity.type.Entity;
-import org.geysermc.geyser.entity.type.ItemFrameEntity;
 import org.geysermc.geyser.entity.type.living.animal.horse.AbstractHorseEntity;
 import org.geysermc.geyser.entity.type.living.animal.horse.LlamaEntity;
 import org.geysermc.geyser.entity.type.player.SessionPlayerEntity;
 import org.geysermc.geyser.entity.vehicle.ClientVehicle;
-import org.geysermc.geyser.level.block.type.Block;
 import org.geysermc.geyser.network.GameProtocol;
 import org.geysermc.geyser.session.GeyserSession;
 import org.geysermc.geyser.translator.protocol.PacketTranslator;
 import org.geysermc.geyser.translator.protocol.Translator;
-import org.geysermc.geyser.translator.protocol.bedrock.BedrockInventoryTransactionTranslator;
 import org.geysermc.geyser.util.CooldownUtils;
-import org.geysermc.mcprotocollib.protocol.data.ProtocolState;
-import org.geysermc.mcprotocollib.protocol.data.game.entity.object.Direction;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.GameMode;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.Hand;
-import org.geysermc.mcprotocollib.protocol.data.game.entity.player.InteractAction;
-import org.geysermc.mcprotocollib.protocol.data.game.entity.player.PlayerAction;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.PlayerState;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundClientTickEndPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.level.ServerboundMoveVehiclePacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundInteractPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundPlayerAbilitiesPacket;
-import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundPlayerActionPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundPlayerCommandPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.player.ServerboundSwingPacket;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Translator(packet = PlayerAuthInputPacket.class)
@@ -81,6 +70,7 @@ public final class BedrockPlayerAuthInputTranslator extends PacketTranslator<Pla
 
         boolean wasJumping = session.getInputCache().wasJumping();
         session.getInputCache().processInputs(entity, packet);
+        session.getBlockBreakHandler().handlePlayerAuthInputPacket(packet);
 
         ServerboundPlayerCommandPacket sprintPacket = null;
 
@@ -92,7 +82,7 @@ public final class BedrockPlayerAuthInputTranslator extends PacketTranslator<Pla
             leftOverInputData.remove(input);
             switch (input) {
                 case PERFORM_ITEM_INTERACTION -> processItemUseTransaction(session, packet.getItemUseTransaction());
-                case PERFORM_BLOCK_ACTIONS -> BedrockBlockActions.translate(session, packet.getPlayerActions());
+                case PERFORM_ITEM_STACK_REQUEST -> session.getPlayerInventoryHolder().translateRequests(List.of(packet.getItemStackRequest()));
                 case START_SWIMMING -> session.setSwimming(true);
                 case STOP_SWIMMING -> session.setSwimming(false);
                 case START_CRAWLING -> session.setCrawling(true);
@@ -230,51 +220,8 @@ public final class BedrockPlayerAuthInputTranslator extends PacketTranslator<Pla
 
     private static void processItemUseTransaction(GeyserSession session, ItemUseTransaction transaction) {
         if (transaction.getActionType() == 2) {
-            int blockState = session.getGameMode() == GameMode.CREATIVE ?
-                session.getGeyser().getWorldManager().getBlockAt(session, transaction.getBlockPosition()) : session.getBreakingBlock();
-
             session.setLastBlockPlaced(null);
             session.setLastBlockPlacePosition(null);
-
-            // Same deal with vanilla block placing as above.
-            if (!session.getWorldBorder().isInsideBorderBoundaries()) {
-                BedrockInventoryTransactionTranslator.restoreCorrectBlock(session, transaction.getBlockPosition());
-                return;
-            }
-
-            Vector3f playerPosition = session.getPlayerEntity().getPosition();
-            playerPosition = playerPosition.down(EntityDefinitions.PLAYER.offset() - session.getEyeHeight());
-
-            if (!BedrockInventoryTransactionTranslator.canInteractWithBlock(session, playerPosition, transaction.getBlockPosition())) {
-                BedrockInventoryTransactionTranslator.restoreCorrectBlock(session, transaction.getBlockPosition());
-                return;
-            }
-
-            int sequence = session.getWorldCache().nextPredictionSequence();
-            session.getWorldCache().markPositionInSequence(transaction.getBlockPosition());
-            // -1 means we don't know what block they're breaking
-            if (blockState == -1) {
-                blockState = Block.JAVA_AIR_ID;
-            }
-
-            LevelEventPacket blockBreakPacket = new LevelEventPacket();
-            blockBreakPacket.setType(LevelEvent.PARTICLE_DESTROY_BLOCK);
-            blockBreakPacket.setPosition(transaction.getBlockPosition().toFloat());
-            blockBreakPacket.setData(session.getBlockMappings().getBedrockBlockId(blockState));
-            session.sendUpstreamPacket(blockBreakPacket);
-            session.setBreakingBlock(-1);
-
-            Entity itemFrameEntity = ItemFrameEntity.getItemFrameEntity(session, transaction.getBlockPosition());
-            if (itemFrameEntity != null) {
-                ServerboundInteractPacket attackPacket = new ServerboundInteractPacket(itemFrameEntity.getEntityId(),
-                    InteractAction.ATTACK, session.isSneaking());
-                session.sendDownstreamGamePacket(attackPacket);
-                return;
-            }
-
-            PlayerAction action = session.getGameMode() == GameMode.CREATIVE ? PlayerAction.START_DIGGING : PlayerAction.FINISH_DIGGING;
-            ServerboundPlayerActionPacket breakPacket = new ServerboundPlayerActionPacket(action, transaction.getBlockPosition(), Direction.VALUES[transaction.getBlockFace()], sequence);
-            session.sendDownstreamGamePacket(breakPacket);
         } else {
             session.getGeyser().getLogger().error("Unhandled item use transaction type!");
             if (session.getGeyser().getLogger().isDebug()) {
