f0b02ffa8ba5344146728659e46dd4896c22ed5f
Eclipse <eclipse@eclipseisoffline.xyz>
2025-09-23 15:02:19 +0000

    Add item tags for 1.21.100 and 1.21.110

--- core/src/main/java/org/geysermc/geyser/registry/populator/TagRegistryPopulator.java
+++ core/src/main/java/org/geysermc/geyser/registry/populator/TagRegistryPopulator.java
@@ -69,11 +69,11 @@ public final class TagRegistryPopulator {
         };
 
         List<ObjectIntPair<String>> paletteVersions = List.of(
+            ObjectIntPair.of("1_21_90", Bedrock_v818.CODEC.getProtocolVersion()),
             // Not a typo, they're the same file
-            ObjectIntPair.of("1_21_70", Bedrock_v818.CODEC.getProtocolVersion()),
-            ObjectIntPair.of("1_21_70", Bedrock_v819.CODEC.getProtocolVersion()),
-            ObjectIntPair.of("1_21_70", Bedrock_v827.CODEC.getProtocolVersion()),
-            ObjectIntPair.of("1_21_70", Bedrock_v843.CODEC.getProtocolVersion())
+            ObjectIntPair.of("1_21_90", Bedrock_v819.CODEC.getProtocolVersion()),
+            ObjectIntPair.of("1_21_100", Bedrock_v827.CODEC.getProtocolVersion()),
+            ObjectIntPair.of("1_21_110", Bedrock_v843.CODEC.getProtocolVersion())
         );
         Type type = new TypeToken<Map<String, List<String>>>() {}.getType();
 
