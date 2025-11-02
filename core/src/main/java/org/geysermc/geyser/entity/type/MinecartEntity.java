cfdf5c2358346ba0b0e0adbc4061fd79c772598f
oryxel1 <gamingnoob681@gmail.com>
2025-11-02 17:56:13 +0700

    Don't lock jumping for minecart.

--- core/src/main/java/org/geysermc/geyser/entity/type/MinecartEntity.java
+++ core/src/main/java/org/geysermc/geyser/entity/type/MinecartEntity.java
@@ -211,6 +211,13 @@ public class MinecartEntity extends Entity implements Tickable {
     }
 
     @Override
+    public boolean doesJumpDismount() {
+        // This is misleading because jumping is literally the only way to dismount for Touch users.
+        // Therefore, do this so we won't lock jumping, prevent Touch user from getting out.
+        return false;
+    }
+
+    @Override
     protected InteractiveTag testInteraction(Hand hand) {
         if (definition == EntityDefinitions.CHEST_MINECART || definition == EntityDefinitions.HOPPER_MINECART) {
             return InteractiveTag.OPEN_CONTAINER;
