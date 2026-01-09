9bea3995c4e46593ee32478c1402ad5fcd620df2
oryxel1 <gamingnoob681@gmail.com>
2026-01-10 01:34:21 +0700

    Move this to AvatarEntity instead.

--- core/src/main/java/org/geysermc/geyser/entity/type/player/AvatarEntity.java
+++ core/src/main/java/org/geysermc/geyser/entity/type/player/AvatarEntity.java
@@ -134,6 +134,17 @@ public abstract class AvatarEntity extends LivingEntity {
         session.sendUpstreamPacket(addPlayerPacket);
     }
 
+    // The player entity already lerp on bedrock, so don't lerp on our side!
+    @Override
+    public void moveAbsolute(Vector3f position, float yaw, float pitch, float headYaw, boolean isOnGround, boolean teleported) {
+        super.moveAbsoluteRaw(position, yaw, pitch, headYaw, isOnGround, teleported);
+    }
+
+    @Override
+    public void moveRelative(double relX, double relY, double relZ, float yaw, float pitch, float headYaw, boolean isOnGround) {
+        super.moveRelativeRaw(relX, relY, relZ, yaw, pitch, headYaw, isOnGround);
+    }
+
     @Override
     public void moveAbsoluteRaw(Vector3f position, float yaw, float pitch, float headYaw, boolean isOnGround, boolean teleported) {
         setPosition(position);
