bab692f9a33f84bccd646fc510f66a6f5ff9bbe2
chris <github@onechris.mozmail.com>
2026-05-26 12:28:14 +0200

    Change POSITION_ADJUSTMENT_MARGIN visibility and comment
    Changed POSITION_ADJUSTMENT_MARGIN to private and updated comment for clarity.

--- core/src/main/java/org/geysermc/geyser/level/physics/CollisionManager.java
+++ core/src/main/java/org/geysermc/geyser/level/physics/CollisionManager.java
@@ -59,8 +59,8 @@ import java.util.Locale;
 public class CollisionManager {
     public static final BlockCollision SOLID_COLLISION = new SolidCollision(null);
     public static final BlockCollision FLUID_COLLISION = new OtherCollision(new BoundingBox[]{new BoundingBox(0.5, 0.25, 0.5, 1, 0.5, 1)});
-    // If you read this, feel free to suggest a more proper way to detect the Bedrock player's own onGround status here
-    public static final double POSITION_ADJUSTMENT_MARGIN = 0.05;
+    // If you read this, feel free to suggest a more proper way to detect the Bedrock player's own onGround status instead of using a margin
+    private static final double POSITION_ADJUSTMENT_MARGIN = 0.05;
 
     private final GeyserSession session;
 
