df6d602f8b2ea7b4d59dffe00ed907b8ca67dc80
onebeastchris <github@onechris.mozmail.com>
2026-06-11 00:30:52 +0200

    Fix 26.20 block palette loading

--- core/src/main/java/org/geysermc/geyser/registry/populator/BlockRegistryPopulator.java
+++ core/src/main/java/org/geysermc/geyser/registry/populator/BlockRegistryPopulator.java
@@ -43,6 +43,7 @@ import org.cloudburstmc.nbt.NbtMap;
 import org.cloudburstmc.nbt.NbtMapBuilder;
 import org.cloudburstmc.nbt.NbtType;
 import org.cloudburstmc.nbt.NbtUtils;
+import org.cloudburstmc.protocol.bedrock.codec.v1001.Bedrock_v1001;
 import org.cloudburstmc.protocol.bedrock.codec.v924.Bedrock_v924;
 import org.cloudburstmc.protocol.bedrock.codec.v944.Bedrock_v944;
 import org.cloudburstmc.protocol.bedrock.codec.v975.Bedrock_v975;
@@ -122,7 +123,7 @@ public final class BlockRegistryPopulator {
                 .put(ObjectIntPair.of("26_0", Bedrock_v924.CODEC.getProtocolVersion()), GoldenDandelionConverter::convert)
                 .put(ObjectIntPair.of("26_10", Bedrock_v944.CODEC.getProtocolVersion()), tag -> tag)
                 .put(ObjectIntPair.of("26_20", Bedrock_v975.CODEC.getProtocolVersion()), tag -> tag)
-                .put(ObjectIntPair.of("26_30", Bedrock_v975.CODEC.getProtocolVersion()), tag -> tag)
+                .put(ObjectIntPair.of("26_30", Bedrock_v1001.CODEC.getProtocolVersion()), tag -> tag)
             .build();
 
         // We can keep this strong as nothing should be garbage collected
