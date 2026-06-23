b340de3c3d6524afab4b8af4c6b9dc39f62fdba4
Novampr <auroranova8756@gmail.com>
2026-06-23 10:42:29 +0100

    Fix integrated server mixin

--- bootstrap/mod/src/main/java/org/geysermc/geyser/platform/mod/mixin/client/IntegratedServerMixin.java
+++ bootstrap/mod/src/main/java/org/geysermc/geyser/platform/mod/mixin/client/IntegratedServerMixin.java
@@ -50,8 +50,8 @@ public class IntegratedServerMixin implements GeyserServerPortGetter {
 
     @Shadow @Final private Minecraft minecraft;
 
-    @Inject(method = "publishServer", at = @At("RETURN"))
-    private void onOpenToLan(GameType gameType, boolean cheatsAllowed, int port, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "publishServer(Lnet/minecraft/server/MinecraftServer$MultiplayerScope;I)Z", at = @At("RETURN"))
+    private void onOpenToLan(MinecraftServer.MultiplayerScope scope, int port, CallbackInfoReturnable<Boolean> cir) {
         if (cir.getReturnValueZ()) {
             // If the LAN is opened, starts Geyser.
             GeyserModBootstrap instance = GeyserModBootstrap.getInstance();
