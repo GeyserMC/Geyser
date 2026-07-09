d3a35dfda1e2f2123002307257c8381c211aaffb
Tyviebrock <stevenrockss13@gmail.com>
2026-07-09 18:16:38 -0400

    Refactor VelocityCommandSource with Gradle setup
    Removed copyright notice and added Gradle plugin configurations.

--- bootstrap/velocity/src/main/java/org/geysermc/geyser/platform/velocity/command/VelocityCommandSource.java
+++ bootstrap/velocity/src/main/java/org/geysermc/geyser/platform/velocity/command/VelocityCommandSource.java
@@ -1,104 +1,91 @@
-/*
- * Copyright (c) 2019-2022 GeyserMC. http://geysermc.org
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @author GeyserMC
- * @link https://github.com/GeyserMC/Geyser
- */
+plugins {
+    id("geyser.platform-conventions")
+    id("geyser.modrinth-uploading-conventions")
+    alias(libs.plugins.runpaper)
+}
 
-package org.geysermc.geyser.platform.velocity.command;
-
-import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.proxy.ConsoleCommandSource;
-import com.velocitypowered.api.proxy.Player;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.geysermc.geyser.command.GeyserCommandSource;
-import org.geysermc.geyser.text.GeyserLocale;
-
-import java.util.Locale;
-import java.util.UUID;
-
-public class VelocityCommandSource implements GeyserCommandSource {
-
-    private final CommandSource handle;
-
-    public VelocityCommandSource(CommandSource handle) {
-        this.handle = handle;
-        // Ensure even Java players' languages are loaded
-        GeyserLocale.loadGeyserLocale(this.locale());
+dependencies {
+    api(projects.core)
+    api(libs.erosion.bukkit.common) {
+        isTransitive = false
     }
 
-    @Override
-    public String name() {
-        if (handle instanceof Player) {
-            return ((Player) handle).getUsername();
-        } else if (handle instanceof ConsoleCommandSource) {
-            return "CONSOLE";
+    implementation(libs.erosion.bukkit.nms) {
+        attributes {
+            attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 21)
         }
-        return "";
     }
 
-    @Override
-    public void sendMessage(@NonNull String message) {
-        handle.sendMessage(LegacyComponentSerializer.legacySection().deserialize(message));
+    implementation(variantOf(libs.adapters.spigot) {
+        classifier("all") // otherwise the unshaded jar is used without the shaded NMS implementations
+    })
+    implementation(variantOf(libs.adapters.paper) {
+        classifier("all") // otherwise the unshaded jar is used without the shaded NMS implementations
+    })
+
+    implementation(libs.cloud.paper)
+    implementation(libs.commodore)
+
+    compileOnly(libs.folia.api)
+
+    compileOnlyApi(libs.viaversion)
+
+    // For 1.16.5/1.17.1
+    implementation(libs.gson.record.factory) {
+        isTransitive = false
+    }
+}
+
+platformRelocate("it.unimi.dsi.fastutil")
+// Relocate net.kyori but exclude the component logger
+platformRelocate("net.kyori", "net.kyori.adventure.text.logger.slf4j.ComponentLogger")
+platformRelocate("org.objectweb.asm")
+platformRelocate("me.lucko.commodore")
+platformRelocate("org.incendo")
+platformRelocate("io.leangen.geantyref") // provided by cloud and Configurate, should also be relocated
+platformRelocate("org.yaml") // Broken as of 1.20
+platformRelocate("org.spongepowered")
+platformRelocate("marcono1234.gson")
+platformRelocate("org.bstats")
+
+provided(libs.viaversion)
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "org.geysermc.geyser.platform.spigot.GeyserSpigotMain"
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+
+    // Prevents Paper 1.20.5+ from remapping Geyser
+    manifest {
+        attributes["paperweight-mappings-namespace"] = "mojang"
     }
 
-    @Override
-    public void sendMessage(Component message) {
-        // Be careful that we don't shade in Adventure!!
-        handle.sendMessage(message);
-    }
+    archiveBaseName.set("Geyser-Spigot")
 
-    @Override
-    public boolean isConsole() {
-        return handle instanceof ConsoleCommandSource;
-    }
+    dependencies {
+        exclude(dependency("com.google.*:.*"))
 
-    @Override
-    public @Nullable UUID playerUuid() {
-        if (handle instanceof Player player) {
-            return player.getUniqueId();
-        }
-        return null;
-    }
+        // Needed because older Spigot builds do not provide the haproxy module
+        exclude("io.netty", libs.netty.codec.haproxy)
 
-    @Override
-    public String locale() {
-        if (handle instanceof Player) {
-            Locale locale = ((Player) handle).getPlayerSettings().getLocale();
-            return GeyserLocale.formatLocale(locale.getLanguage() + "_" + locale.getCountry());
-        }
-        return GeyserLocale.getDefaultLocale();
+        // Commodore includes Brigadier
+        exclude(dependency("com.mojang:.*"))
     }
+}
 
-    @Override
-    public boolean hasPermission(String permission) {
-        // Handle blank permissions ourselves, as velocity only handles empty ones
-        return permission.isBlank() || handle.hasPermission(permission);
-    }
+modrinth {
+    uploadFile.set(tasks.getByPath("shadowJar"))
+    gameVersions.addAll("1.16.5", "1.17", "1.17.1", "1.18", "1.18.1", "1.18.2", "1.19",
+        "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+        "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10",
+        "1.21.11", "26.1", "26.1.1", "26.1.2")
+    loaders.addAll("spigot", "paper")
+}
 
-    @Override
-    public Object handle() {
-        return handle;
+tasks {
+    runServer {
+        minecraftVersion(libs.versions.runpaperversion.get())
+        jvmArgs("-Dcom.mojang.eula.agree=true")
     }
 }
