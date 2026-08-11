9170773d5440b4208c8595f8b37cdccdb4a7469c
darkcupid412 <darkcupid412@gmail.com>
2026-08-11 12:18:50 +0545

    Use the skybox clock for custom minecraft-namespace dimensions too

--- core/src/main/java/org/geysermc/geyser/level/JavaDimension.java
+++ core/src/main/java/org/geysermc/geyser/level/JavaDimension.java
@@ -66,22 +66,22 @@ public record JavaDimension(int minY, int height, boolean piglinSafe, boolean ul
         // Cache the Bedrock version of this dimension, and base it off the ID - THE ID CAN CHANGE!!!
         // https://github.com/GeyserMC/Geyser/issues/4837
         int bedrockId;
-        // The dimension a custom dimension resembles is also the one whose clock it should follow
+        // Effects should give is a clue on how this (custom) dimension is supposed to look like
+        String skyboxId = switch (dimension.getString("skybox")) {
+            case "none" -> "minecraft:the_nether";
+            case "end" -> "minecraft:the_end";
+            default -> "minecraft:overworld";
+        };
+        // The dimension a custom dimension resembles is also the one whose clock it should follow.
+        // Note that custom dimension types can also live in the minecraft namespace.
         Key clockId;
         Key id = entry.id();
         if ("minecraft".equals(id.namespace())) {
             String identifier = id.asString();
             bedrockId = DimensionUtils.javaToBedrock(identifier);
             isNetherLike = BedrockDimension.NETHER_IDENTIFIER.equals(identifier);
-            clockId = id;
+            clockId = COMMON_CLOCKS.containsKey(id) ? id : MinecraftKey.key(skyboxId);
         } else {
-            // Effects should give is a clue on how this (custom) dimension is supposed to look like
-            String skybox = dimension.getString("skybox");
-            String skyboxId = switch (skybox) {
-                case "none" -> "minecraft:the_nether";
-                case "end" -> "minecraft:the_end";
-                default -> "minecraft:overworld";
-            };
             bedrockId = DimensionUtils.javaToBedrock(skyboxId);
             isNetherLike = BedrockDimension.NETHER_IDENTIFIER.equals(skyboxId);
             clockId = MinecraftKey.key(skyboxId);
