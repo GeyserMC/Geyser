6e5db75922269c08b2ce0f0ec2a4dae061266b1b
Callum <114112457+TotallyNoCallum@users.noreply.github.com>
2026-08-21 02:15:43 +0100

    accidentally put 2.11.2 in @since

--- api/src/main/java/org/geysermc/geyser/api/connection/GeyserConnection.java
+++ api/src/main/java/org/geysermc/geyser/api/connection/GeyserConnection.java
@@ -179,7 +179,7 @@ public interface GeyserConnection extends Connection, CommandSource {
      * @param emoteId the emote ID to send to this client.
      * @param silent whether to silence the emote chat announcement.
      *
-     * @since 2.11.2
+     * @since 2.11.1
      */
     void showEmote(GeyserAvatarEntity emoter, String emoteId, boolean silent);
 
