41de8b2cb4d94dad5e82fe22a03961d65c261773
Nia <darkcupid412@gmail.com>
2026-09-07 01:04:11 +0545

    Update api/src/main/java/org/geysermc/geyser/api/block/custom/component/MaterialInstance.java
    Co-authored-by: chris <github@onechris.mozmail.com>

--- api/src/main/java/org/geysermc/geyser/api/block/custom/component/MaterialInstance.java
+++ api/src/main/java/org/geysermc/geyser/api/block/custom/component/MaterialInstance.java
@@ -66,7 +66,7 @@ public interface MaterialInstance {
      * @return If the block should have ambient occlusion.
      * @deprecated Use {@link #ambientOcclusionExponent()} instead
      */
-    @Deprecated(since = "2.11.1")
+    @Deprecated(since = "2.11.3")
     boolean ambientOcclusion();
 
     /**
