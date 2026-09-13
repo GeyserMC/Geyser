4d85d08403c1769790ab7b9fdb453f5bdff43dc7
huda <hawda@exient.net>
2026-09-13 19:43:10 +0300

    Optimize custom skull allocations

--- core/src/main/java/org/geysermc/geyser/registry/type/CustomSkull.java
+++ core/src/main/java/org/geysermc/geyser/registry/type/CustomSkull.java
@@ -50,7 +50,6 @@ public class CustomSkull {
     private static final String BITS_B_PROPERTY = "geyser_skull:bits_b";
 
     private static final int[] ROTATIONS = {0, -90, 180, 90};
-    
     private static final String[] QUADRANT_NAMES = {"a", "b", "c", "d"}; 
 
     private static final BoxComponent FLOOR_BOX = new BoxComponent(
@@ -127,9 +126,9 @@ public class CustomSkull {
     }
 
     private void addFloorPermutations(List<CustomBlockPermutation> permutations) {
+
         for (int quadrant = 0; quadrant < 4; quadrant++) {
             for (int i = 0; i < 4; i++) {
-
                 int floorRotation = 4 * quadrant + i;
                 CustomBlockComponents components = new GeyserCustomBlockComponents.Builder()
                         .selectionBox(FLOOR_BOX)
